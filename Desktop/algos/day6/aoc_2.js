eb443c00a40968c35397a4c86b299ca895d18923
rvsin8 <rsingh0815@gmail.com>
2020-12-10 11:47:25 -0500

    day6

--- Desktop/algos/day6/aoc_2.js
+++ Desktop/algos/day6/aoc_2.js
@@ -10,30 +10,17 @@ const solve = async () => {
     let total = 0; 
     for (let group of groups) { //iterate through the group
         const lines = group.split('\n'); //split on the new line char
-        let unionSet = new Set(); //set the union set into an empty set
+        let intersectionSet = new Set(lines[0]); //set the union set into an empty set, if we start wit no items in this - we will always get 0 so set it to the first line bc every group will have a line to it
         for (let line of lines) { //iterate through the line in a group
             const lineSet = new Set(line); //union this line set to the union set
-            unionSet = union(unionSet, lineSet); //union both
+            intersectionSet = intersection(intersectionSet, lineSet); //union both
         }
 
-        total += unionSet.size; //we find the size of the union 
+        total += intersectionSet.size; //we find the size of the union 
     }
     return total;
 };
 
-
-const union = (set1, set2) => {
-    const newSet = new Set(); //return a new set that equals an empty set
-    for (let item of set1) { //iterate through the first set and add items
-        newSet.add(item);
-    }
-
-    for (let item of set2) { //iterate through second set and add items
-        newSet.add(item);
-    }
-
-    return newSet;
-};
 
 const intersection = (set1, set2) => { //helper method
     const newSet = new Set(); //set newSet into an empty set first
