3121a49084daeaf6c42ee968bc3565d6f242945c
rvsin8 <rsingh0815@gmail.com>
2020-12-11 13:28:05 -0500

    day7

--- Desktop/algos/day7/aoc_2.js
+++ Desktop/algos/day7/aoc_2.js
@@ -1,3 +1,4 @@
+const { count } = require('console');
 const { read } = require('fs');
 
 const fs = require('fs').promises;
@@ -12,56 +13,51 @@ const solve = async () => {
     const graph = {}; //nodes will be keys of this object
     for (let line of lines) { //iterate through each line 
         const { destination, sources } = parseLine(line); //get parse line for every line -- we will get back an object with a destination and an array of sources
-        if (!(destination in graph)) //the first time i see a node, i want to seee if its in the graph if not in the graph 
-            graph[destination] = []; //we can add it to the graph
+        graph[destination] = sources; // will give us the # as a source
 
-        for (let source of sources){ //iterate through all source nodes & key into them
-            if (!(source in graph)) //if its not in there
-                graph[source] = []; //initialize them
-            graph[source].push(destination); //graph at the source points to the destination
-        }
     }
-    return traverse(graph, 'shiny gold bag') - 1;
+    return traverse (graph, 'shiny gold bag') - 1; //to get off by 1 count
 };
 
 
-const traverse = (graph, node, visited = new Set()) => { //revisit this method
-    if (visited.has(node))
-        return 0;
-
-    visited.add(node);
-
-    let numBagColors = 1; //we want a count of things, lets us know what current bag we're at
-    for (let neighbor of graph[node]) { //visit neighbors from my current node
-        numBagColors += traverse(graph, neighbor, visited); //increase it by the num value of this recursive call
+const traverse = (graph, node) => { //revisit this method
+    let count = 1; //set count
+    for (let neighbor in graph[node]){ //touch every node of the graph, neighbor is going to be "light lavender"
+        const qty = graph[node][neighbor]; // what is the label on the edge that connects hte node to the neighbor
+        count += qty * traverse(graph, neighbor); //finding the number of bags in my neighbor's subtree and multiplying it by the quantity that i have of the neighbor
     }
-
-    return numBagColors;
+    return count; //return final quantity/answer
 };
 
 
 
 const parseLine = (line) => {
   const [ destination, rest ] = line.split('s contain ');
+  if (rest.slice(0, 3) === 'no ') {
+    return {
+      destination,
+      sources: {}
+    };
+  }
 
   const sourceSegments = rest.split(', ');
-  const sources = [];
+  const sources = {};
   for (let i = 0; i < sourceSegments.length; i += 1) {
     const segment = sourceSegments[i];
     const amount = Number(segment[0]);
     let source = amount === 1 ? segment.slice(2) : segment.slice(2, -1);
-
+    
     if (i === sourceSegments.length - 1)
       source = source.slice(0, -1);
 
-    sources.push(source);
+    sources[source] = amount;
   }
+
   return {
     destination,
     sources
   };
 };
 
-//console.log(parseLine('dark turquoise bags contain 4 dark bronze bags, 3 posh tan bags'));
 
 solve().then(console.log); // 101
