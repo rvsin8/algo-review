b0e09b7c913fcfbbd7862358d05b0415aa8bbe0c
rvsin8 <rsingh0815@gmail.com>
2020-12-12 02:35:47 -0500

    day8

--- Desktop/algos/day8/aoc_1.js
+++ Desktop/algos/day8/aoc_1.js
@@ -9,22 +9,28 @@ const solve = async () => {
     const lines = await readLines(); 
     const instructions = lines.map(parseInstruction); //map instructions line by line
     //console.table(instructions); get a table
+    return getAcc(instructions);
     
 };
 
-const getAcc = (instructions, i = 0 ) => { //we will begin at 0 as our acc
+const getAcc = (instructions, i = 0, visited = new Set() ) => { //we will begin at 0 as our acc //visited being put in a set makes for quicker search time bc its a hash data structure
+    if (visited.has(i)) //if i seen this instruction before, then i have a base case, if it is return that value
+        return 0;
+
+        visited.add(i); //make sure instructions are marked as visited
+    
     const { type, val } = instructions[i];
 
     //jmp
     //acc
     //nop
     if (type === 'jmp'){ //we need conditionals for each
-        return getAcc(instructions, i + val); //jmp is i(current instruction im at) + val presented and adding it to our current i
+        return getAcc(instructions, i + val, visited); //jmp is i(current instruction im at) + val presented and adding it to our current i
 
     } else if (type === 'acc'){
-        return val + getAcc(instructions, i + val); //my value plus what my recursive call returns
+        return val + getAcc(instructions, i + 1, visited); //relook this //my value plus what my recursive call returns
     } else {
-        return getAcc(instruction, i + 1); //proceed to the very next instruction
+        return getAcc(instructions, i + 1, visited); //proceed to the very next instruction
     }
 
 };
