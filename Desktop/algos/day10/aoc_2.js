95eff804faf4b89b95e57ca4f1cb016d48513983
rvsin8 <rsingh0815@gmail.com>
2020-12-12 18:22:24 -0500

    day10

--- Desktop/algos/day10/aoc_2.js
+++ Desktop/algos/day10/aoc_2.js
@@ -1,3 +1,59 @@
+--- Part Two ---
+To completely determine whether you have enough adapters, you'll need to figure out how many different ways they can be arranged. Every arrangement needs to connect the charging outlet to your device. The previous rules about when adapters can successfully connect still apply.
+
+The first example above (the one that starts with 16, 10, 15) supports the following arrangements:
+
+(0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
+(The charging outlet and your device's built-in adapter are shown in parentheses.) Given the adapters from the first example, the total number of arrangements that connect the charging outlet to your device is 8.
+
+The second example above (the one that starts with 28, 33, 18) has many arrangements. Here are a few:
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 48, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 48, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 47, 48, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+46, 48, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+46, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+47, 48, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+47, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+48, 49, (52)
+In total, this set of adapters can connect the charging outlet to your device in 19208 distinct arrangements.
+
+You glance back down at your bag and try to remember why you brought so many adapters; there must be more than a trillion valid ways to arrange them! Surely, there must be an efficient way to count the arrangements.
+
+What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?
+
+Your puzzle answer was 10578455953408.
+
+
+
 const fs = require('fs').promises;
 
 const readLines = async () => {
@@ -8,27 +64,33 @@ const readLines = async () => {
 const solve = async () => {
     const lines = await readLines();
     const numbers = lines.map(Number); //we want the strings coverted into numbers
-    const sorted = numbers.sort((a,b) => a - b ); //in increasing order
+    const sorted = numbers.sort((a,b) => a - b); //in increasing order
     //console.log(sorted)
     const last = sorted[sorted.length - 1]; //to fix off by one error thats why we do this
     const input = [0, ...sorted, last + 3]; //our true input with 0, sorted array and last element + 3
-    numWays(input); //function for our recursive call
+    return numWays(input); //function for our recursive call
 }; 
 
-const numWays = (array, i = 0) => { //take in the array, we need to know our current position so we set i at index 0 of the array
+const numWays = (array, i = 0, memo = {}) => { //take in the array, we need to know our current position so we set i at index 0 of the array
+    if (i in memo)
+        return memo[i];
+
     if (i === array.length - 1) //if my index is last position that means we found a successful way which would be 1
         return 1;
 
     let total = 0;
 
     if (array[i + 1] && ((array[i + 1] - array[i]) <= 3)) //if my next pos is in bounds & my next position has a pos that is less than or equal to 3 
-        total += numWays(array, i + 1);//then make that recursive call
+        total += numWays(array, i + 1, memo);//then make that recursive call
 
     if (array[i + 2] && ((array[i + 2] - array[i]) <= 3)) //if my next pos is in bounds & my next position has a pos that is less than or equal to 3 
-        total += numWays(array, i + 2);//then make that recursive call
+        total += numWays(array, i + 2, memo);//then make that recursive call
 
     if (array[i + 3] && ((array[i + 3] - array[i]) <= 3)) //if my next pos is in bounds & my next position has a pos that is less than or equal to 3 
-        total += numWays(array, i + 3);//then make that recursive call
+        total += numWays(array, i + 3, memo);//then make that recursive call
+
+    memo[i] = total;
+    return total;
 
 };
 
