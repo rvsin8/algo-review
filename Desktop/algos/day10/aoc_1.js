f6129f3781f05dba64c46cccb38be6819deeeab3
rvsin8 <rsingh0815@gmail.com>
2020-12-12 15:51:25 -0500

    day10

---
+++ Desktop/algos/day10/aoc_1.js
@@ -0,0 +1,16 @@
+const fs = require('fs').promises;
+
+const parseLines = async () => {
+  const data = await fs.readFile('./input', {encoding: 'utf-8'});
+  return data.split('\n');
+};
+
+const solve = async () => {
+    const lines = await parseLines();
+
+
+    
+}; 
+
+solve().then(console.log); //955584
+
