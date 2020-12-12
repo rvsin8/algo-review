9f0b77bf782c8384e681a278561197a4a9c20413
rvsin8 <rsingh0815@gmail.com>
2020-12-12 18:12:16 -0500

    day10

--- Desktop/algos/day10/aoc_2.js
+++ Desktop/algos/day10/aoc_2.js
@@ -16,8 +16,19 @@ const solve = async () => {
 }; 
 
 const numWays = (array, i = 0) => { //take in the array, we need to know our current position so we set i at index 0 of the array
-    if (i === array.length - 1) //
+    if (i === array.length - 1) //if my index is last position that means we found a successful way which would be 1
         return 1;
+
+    let total = 0;
+
+    if (array[i + 1] && ((array[i + 1] - array[i]) <= 3)) //if my next pos is in bounds & my next position has a pos that is less than or equal to 3 
+        total += numWays(array, i + 1);//then make that recursive call
+
+    if (array[i + 2] && ((array[i + 2] - array[i]) <= 3)) //if my next pos is in bounds & my next position has a pos that is less than or equal to 3 
+        total += numWays(array, i + 2);//then make that recursive call
+
+    if (array[i + 3] && ((array[i + 3] - array[i]) <= 3)) //if my next pos is in bounds & my next position has a pos that is less than or equal to 3 
+        total += numWays(array, i + 3);//then make that recursive call
 
 };
 
