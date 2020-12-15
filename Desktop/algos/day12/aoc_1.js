d9bb23715094d7975a216e130851bfc262da1d29
rvsin8 <rsingh0815@gmail.com>
2020-12-14 20:12:56 -0500

    day12

--- Desktop/algos/day12/aoc_1.js
+++ Desktop/algos/day12/aoc_1.js
@@ -45,6 +45,20 @@ const calcCardinal = (commands) => { //commands are just the commands with N S E
     return ship;
 };
 
+const calcRotation = (commands) => {
+    const ship = { x: 0, y: 0, angle: 90 }; //where the ship begins at origin //n is 0 e is 90 s is 180 w is 360
+    for (let command of commands) { //we want to look into every single command
+        const { type, val } = command; //type of command the number value and type
+        if (type === 'L'){ //if my type equal north
+            ship.angle = (360 + ship.angle + val) % 360; //reverse order //no matter what we increase by 360 we wll get a positive value instead of a negative one
+        } else if (type === 'R'){ //if my type equal north
+            ship.angle = (ship.angle + val) % 360; //we have to keep it in respects to 360 so we mod, we need to add whatever value we arrive at to the original value we have
+        }
+        console.log(ship);
+    }
+};
+
+calcRotation([{type:'L', val: 180}])
 
 
 
