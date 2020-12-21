d04a43754cb35b2f2360f3a45016806d8afd4163
rvsin8 <rsingh0815@gmail.com>
2020-12-21 17:14:22 -0500

    day17

--- Desktop/algos/day17/aoc_1.js
+++ Desktop/algos/day17/aoc_1.js
@@ -9,7 +9,7 @@ const solve = async () => {
     const lines = await readLines();
     //console.log(lines);
     const initialnodes = getActiveNodes(lines);
-    console.log(initialnodes)
+    //console.log(initialnodes)
 
     
 }; 
@@ -29,12 +29,33 @@ const getActiveNodes = (lines) => {
     return nodes; //return that set of nodes
 };
 
-const getNodeFromArray = (array) => { //3 coord of an array
-    return array.join(',');
+const getNodeFromArray = (triplet) => { //3 coord of an array
+    return triplet.join(',');
 
 };
 
-console.log(getNodeFromArray([3, 10, 2]));
+
+const getNeighbors = (triplet) => {
+    const [x, y, z] = triplet; //pluck out the original x y z
+    const neighbors = new Set(); //keep it in a new set
+
+    for (let deltaX = -1; deltaX <= 1; deltaX += 1){
+        for (let deltaY = -1; deltaY <= 1; deltaY += 1){
+            for (let deltaZ = -1; deltaZ <= 1; deltaZ += 1){
+                const neighbor = getNodeFromArray([x + deltaX, y + deltaY, z + deltaZ]);
+                neighbors.add(neighbor);
+            }
+        }
+
+
+    }
+    neighbors.delete(getNodeFromArray(triplet));
+    return neighbors;
+};
+
+
+
+console.log(getNeighbors([0, 0, 0]));
 
 solve().then(console.log); 
 
