9bb0832eda255ec2228ef5a6f7bee4454f0e4f35
rvsin8 <rsingh0815@gmail.com>
2020-12-25 00:49:10 -0500

    longest_substr_no

--- Desktop/leetcode/google/arrays_strings/longest_substr_wo_recurring.js
+++ Desktop/leetcode/google/arrays_strings/longest_substr_wo_recurring.js
@@ -28,3 +28,29 @@
 //
 //0 <= s.length <= 5 * 104
 //s consists of English letters, digits, symbols and spaces.
+
+
+var lengthOfLongestSubstring = function(s) {
+    if (!s) return 0; //if the string is empty return 0
+    if (s.length < 2) return s.length; //if the string is less than 2 return the original string bc we are looking for re-occurring chars 
+    var ls = s[0]; // longest string
+    var cs = s[0]; // current string
+    for (var i = 1; i < s.length; i++) { //iterate through the index
+        var index = cs.indexOf(s[i]); // get index of current character in current string and save it in a variable 
+        if (index > -1) { // found the character in current string
+            if (cs.length > ls.length) { //if the length of the current string is greater than the longest string - replace it
+                ls = cs; // update longest string, replace it
+            }
+            cs = cs.substring(index + 1,cs.length) + s[i]; // remove the first part of the string which contains repeated character //use this for the next longest string in the sentence
+        } else {
+            cs = cs + s[i];  //if not return the string count
+        }        
+    }
+    if (cs.length > ls.length) { //if the length of the current string is greater than the longest string - replace it
+        ls = cs;                
+    }
+    return ls.length;
+};
+
+
+//make sense out of this line by line
