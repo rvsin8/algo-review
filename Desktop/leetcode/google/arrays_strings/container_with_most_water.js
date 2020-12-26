4159d206ee03c51f69908c4ff9b1dadf4ecd2836
rvsin8 <rsingh0815@gmail.com>
2020-12-25 19:45:39 -0500

    water container

--- Desktop/leetcode/google/arrays_strings/container_with_most_water.js
+++ Desktop/leetcode/google/arrays_strings/container_with_most_water.js
@@ -11,24 +11,27 @@
 //Hint 2 
 //Start with the maximum width container and go to a shorter width container if there is a vertical line longer than the current containers shorter line. This way we are compromising on the width but we are looking forward to a longer length container.
 
-public int maxArea(int[] heights) {
-    if(heights.length < 2){
-         return 0;
-    }
-    int maxArea = 0;
-    for (int i = 0; i < heights.length; i++) {
-        for (int j = i + 1; j < heights.length; j++) {
-            int height = heights[i];
-            int width = j - i;
-            if(height > heights[j]){
-                height = heights[j];
-            }
-            int area = height * width;
-            if (area > maxArea) {
-                maxArea = area;
-            }
+//the input is going to be an array of numbers and each index the number represents the height of each wall
+//the goal is to return the container with the most water - the heigh of the red wall is 7 and the width is 7 the area is then 7 * 7 = 49
+//we need to use ratcheting to solve this problem --> a pointer at the first index and end index, at each stage we are either going decrement the ending pointer or increase the starting pointer
+//we want to keep track of the height, width and area of the current container
+//at the end of the algorithm we will return the maximum area we have seen
+//we always want to keep the higher height of thw two walls bc that will give us our highest max area, from there we decide what to increment or decrement
+//when we have the same height how do we know which one we need to decrement ? it does not really matter. *check out explanation*
 
-        }
-    }
-    return maxArea;
+//first iteration: height is 1, width is 8, area us 8
+//second iteration: height is 7, width is 7, area 49
+//third iteration:  height is 3, width is 6, area is 18
+//fourth iteration: height is 8, width is 5, area is 40
+//fifth iteration: height is 4, width is 4, area is 16
+//sixth iteration: height is 5, width is 3, area is 15
+//seventh iteration: height is 2, width is 2, area is 4
+//eighth iteration: height is 6, width is 1, area is 6
+//time complexity is O(n), because we are ratcheting from the end to the start until those two points meet and once they meet we are done, they only run once and we are using only the input array and no extra space
+
+
+function maxArea(a){
+
+
 }
+
--- Desktop/leetcode/google/arrays_strings/three_sums.js
+++ Desktop/leetcode/google/arrays_strings/three_sums.js
@@ -37,30 +37,38 @@
 //The second train of thought for two-sum is, without changing the array, can we use additional space somehow? Like maybe a hash map to speed up the search?
 
 
-var threeSum = function(nums) {
-    let res = []
-    nums.sort((a,b) => a - b)
+function threeSum(nums) {
+    let result = [];
+    let curResult = [];;
+    
+    let left = 0;
+    let right = nums.length - 1;
+    
+    // sorting least to greatest
+    nums.sort((a, b) => a - b); 
+    
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === nums[i - 1])
-            continue
-        let target = -nums[i]
-        let left = i + 1, right = nums.length - 1
-        while (left < right) {
-            if (nums[left] + nums[right] === target) {
-                res.push([nums[left], -target, nums[right]])
-                while (nums[left] === nums[left + 1])
-                    left++
-                left++
-                while (nums[right] === nums[right - 1])
-                    right--
-                right--
-            }
-            else if (nums[left] + nums[right] > target)
-                right--
-            else
-                left++
-        }
+        if(i > 0 && nums[i] === nums[i - 1]) continue;
         
+        left = i + 1;
+        right = nums.length - 1;
+        let sum = 0;
+        
+        while (left < right) {
+            sum = nums[i] + nums[left] + nums[right];
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+                left++;
+                right--;
+                
+                while(left < right && nums[left] === nums[left - 1]) left++;
+                while(left < right && nums[right] === nums[right + 1]) right++;
+            } else if (sum > 0) {
+                right--;
+            } else {
+                left++;
+            }
+        }
     }
-    return res
-};
+    return result;
+}
