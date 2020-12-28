28425b30febeea98115e7703609cf63bee6ced57
rvsin8 <rsingh0815@gmail.com>
2020-12-28 18:02:52 -0500

    subarray sort

--- Desktop/algoexpert/hard/subarray_sort.js
+++ Desktop/algoexpert/hard/subarray_sort.js
@@ -10,3 +10,12 @@
 //[1,2,3,4,5,-1] --> what will the extremities for our lowest and highest unsorted num
 //[1,2,4,3,5,-1] --> we still have to sort the entire array as the example above
 //if we want to find that subarray to sort everything in that array, we need to find every unsorted number and if there is one there has to be another, we need to find the greatest one and the smallest one
+
+//algorithm
+//we can start by iterating through our array, at each num determine if its out of order by comparing it to its adjacent numbers
+//in the first example above 11 is unsorted, naturally the num after 11 is unsorted - 7
+//so is 12 - so is 6
+//11,7,12,6 are unsorted, lowest num is 6 and largest num is 12 after its sorted it would be idx 3 and 9
+//we start from the left and 6 should be coming after 1,2,4 but before 7 which is idx 3
+//12 we will start from the right, behind 19,18,16 but after 7 so idx 9.
+//sort from idx3 to idx9
