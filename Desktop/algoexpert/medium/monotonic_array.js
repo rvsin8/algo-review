3a8db505d7e840b142b1043dfdedcaa2029c48d3
rvsin8 <rsingh0815@gmail.com>
2020-12-29 13:55:17 -0500

    monotonic array

--- Desktop/algoexpert/medium/monotonic_array.js
+++ Desktop/algoexpert/medium/monotonic_array.js
@@ -21,26 +21,39 @@
 //space complexity
 //O(1) we are just keeping track of two variable
 
+//
+//function isMonotonic(array){ //take in an array
+//    if (array.length <= 2) return true; 
+//
+//    let direction = array[1] - array[0];
+//    for (let i = 2; i < array.length; i++) { //we will 
+//        if (direction === 0){
+//            direction = array[i] - array[i - 1];
+//            continue;
+//        }
+//        if (breaksDirection(direction, array[i-1], array[i])){
+//            return false;
+//        }
+//    }
+//
+//    return true;
+//}
+//
+//function breaksDirection(direction, previousInt, currentInt) {
+//    const difference = currentInt - previousInt;
+//    if (direction > 0) return difference < 0;
+//    return difference > 0;
+//}
+//
 
-function isMonotonic(array){
-    if (array.length <= 2) return true;
+function isMonotonic(array){ //take in an array
+    let isNonDecreasing = true; //first var, if its trending upwards set to true
+    let isNonIncreasing = true; //sec var, if its trending downwards set it to true
 
-    let direction = array[1] - array[0];
-    for (let i = 2; i < array.length; i++) {
-        if (direction === 0){
-            direction = array[i] - array[i - 1];
-            continue;
-        }
-        if (breaksDirection(direction, array[i-1], array[i])){
-            return false;
-        }
+    for(let i = 1; i < array.length; i++){ //we start from idx 1 bc we need to compare the sec ele to the first and iterate through the length of the array
+        if (array[i] < array[i - 1]) isNonDecreasing = false; //if the first ele is less than the sec then it is NOT non-decreasing
+        if (array[i] > array[i - 1]) isNonIncreasing = false; //if the first ele is greater than the sec than it is NOT non-increasing
     }
 
-    return true;
-}
-
-function breaksDirection(direction, previousInt, currentInt) {
-    const difference = currentInt - previousInt;
-    if (direction > 0) return difference < 0;
-    return difference > 0;
+    return isNonDecreasing || isNonIncreasing; //return whatever boolean returns
 }
