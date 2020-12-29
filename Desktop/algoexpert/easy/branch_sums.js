6381e08749487c22fc1faf6d1f6e811de7908e75
rvsin8 <rsingh0815@gmail.com>
2020-12-29 02:24:16 -0500

    branch sums

--- Desktop/algoexpert/easy/branch_sums.js
+++ Desktop/algoexpert/easy/branch_sums.js
@@ -11,9 +11,34 @@
 //again call it recursively with the running sum being now 7, 8, 10, 11
 //running sum 15, 16, 18 once we hit the leaf nodes
 //we will pass down an array and fill it up once we hit the leaf nodes [15,16,18,10,11]
+//we need to know how many branch sums we have bc it will dictate the length of the array, you will never have more than n branch sums bc you have n nodes
+
 
 //time complexity
 //O(N) because we have to traverse through all of the nodes to account for each of their values, at every node we are just doing constant time operations like adding or checking if nodes exists
 
 //space complexity
 //recursively, we will have multiple calls on the callstack O = Log(N) bc you are eliminating half of the nodes as you move around
+//O(N) //review this it got confusing
+
+function branchSums(root){//
+    const sums = [];//keep track of our running sum in a list
+    calculateBranchSums(root, 0, sums);
+    return sums;
+}
+
+function calculateBranchSums(node, runningSum, sums){//
+    if (!node) return;
+
+    const newRunningSum = runningSum + node.value;
+    if (!node.left && !node.right){
+        sums.push(newRunningSum);
+        return;
+    }
+
+    calculateBranchSums(node.left, newRunningSum, sums);
+    calculateBranchSums(node.right, newRunningSum, sums);
+}
+
+
+
