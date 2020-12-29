4fb7b071a60ad0c53b4caa5d5f6bcb786f564379
rvsin8 <rsingh0815@gmail.com>
2020-12-28 22:54:06 -0500

    find closest value to bst

--- Desktop/algoexpert/easy/find_closest_value_bst.js
+++ Desktop/algoexpert/easy/find_closest_value_bst.js
@@ -1,2 +1,9 @@
 //Find Closest Value in BST
 //binary Search Tree
+
+//notes
+//given a bst and a target int value, we find the closest value to the target value in the bst
+//using logic similar to insertion and removal
+//we want to initialize a variable that will keep track of the closest value in that bst and continualy update it
+//in the beginning we will have the closest val equal to infinity, you can initialize it to whatever and this var will hold the best candidate for the closest value
+//we start at the root node and compare this value to the target value
