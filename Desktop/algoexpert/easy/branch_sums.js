6f2ca42c63f6cbb939c5c50114cb59c6ccdb4bb5
rvsin8 <rsingh0815@gmail.com>
2020-12-29 02:12:17 -0500

    branch sums

--- Desktop/algoexpert/easy/branch_sums.js
+++ Desktop/algoexpert/easy/branch_sums.js
@@ -9,4 +9,11 @@
 //our root node has 2 children nodes, we would call a recursive call on each of these nodes while keeping track of the running sum 1 and 1
 //we are keeping going down with the running sum being 3 and 4
 //again call it recursively with the running sum being now 7, 8, 10, 11
-//running sum 15, 16, 18, 
+//running sum 15, 16, 18 once we hit the leaf nodes
+//we will pass down an array and fill it up once we hit the leaf nodes [15,16,18,10,11]
+
+//time complexity
+//O(N) because we have to traverse through all of the nodes to account for each of their values, at every node we are just doing constant time operations like adding or checking if nodes exists
+
+//space complexity
+//recursively, we will have multiple calls on the callstack O = Log(N) bc you are eliminating half of the nodes as you move around
