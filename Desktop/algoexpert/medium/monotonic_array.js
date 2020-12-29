5054b1451cf0bd42ad09ca8bf6592dbdb769e8fc
rvsin8 <rsingh0815@gmail.com>
2020-12-29 12:46:59 -0500

    monotonic array

---
+++ Desktop/algoexpert/medium/monotonic_array.js
@@ -0,0 +1,10 @@
+//Monotonic Array
+//Arrays
+
+//notes
+//this question gives you an array of integers and asks you if that array is monotonic
+//if we take all the integers in that array and read it from left to right or 
+//to be monotonic is can either be entirely non-increasing or entirely non-decreasing so you are checking for two coniditions
+//monotonic integers can have an array that have two int that are equal to each other
+//[-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
+
