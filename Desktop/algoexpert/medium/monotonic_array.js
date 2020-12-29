c7e9ec1e7cb6adaa70d4ed019bc5752735408579
rvsin8 <rsingh0815@gmail.com>
2020-12-29 13:06:24 -0500

    monotonic array

--- Desktop/algoexpert/medium/monotonic_array.js
+++ Desktop/algoexpert/medium/monotonic_array.js
@@ -4,7 +4,17 @@
 //notes
 //this question gives you an array of integers and asks you if that array is monotonic
 //if we take all the integers in that array and read it from left to right or 
-//to be monotonic is can either be entirely non-increasing or entirely non-decreasing so you are checking for two coniditions
+//to be monotonic is can either be entirely non-increasing or entirely non-decreasing so you are checking for two conditions
 //monotonic integers can have an array that have two int that are equal to each other
 //[-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
+//direction for first two ele [-1, -5] is going down
+//[-5, -10] direction is also going down
+//[-10, -1100] direction is also going down and so on
 
+
+
+//time complexity
+//O(N), probably not a better one than this bc we have to iterate through each ele in the array
+
+//space complexity
+//O(1)
