f9d58211b2ca9f007eb39e08a8872f6ea383d6f3
rvsin8 <rsingh0815@gmail.com>
2020-12-28 23:15:32 -0500

    find closest value to bst

--- Desktop/algoexpert/easy/find_closest_value_bst.js
+++ Desktop/algoexpert/easy/find_closest_value_bst.js
@@ -13,3 +13,16 @@
 //now we go through the right side of the tree to 15, 15-12 = 3, 3 > 2 so that means 15 is farther away from 12 than 10 is so we do not update 
 //so now we compare 12 and 15, we know that 12 is less than 15 so 22 is eliminated aka half the bst and on avg
 //now we look at the left of 15, 13 - 12 = 1 and that is less than 2 so we update the bst to 13 as the current closest
+//now we for the last time compare 13 to 12, 12 is smaller so we are going to eliminate the the right side of that bst and we are done
+
+//edge case
+//if we ever get to the point if the abs diff is 0, we can stop there.
+
+//time complexity
+//O(log(N)) where N is the number of nodes in our tree bc we are eliminating on avg half the tree every time we search a node
+//worst case is O(N) is when we have a tree with one branch, we can't take adv of the bst property bc we can't eliminate half the tree, we would have to go down the entire tree
+
+
+//space complexity
+//recursive will be O(log(N)), we would be calling the findBST value multiple times and so we are adding frames on the call stack and using extra memory
+//worst O(N) N if theres one branch so we call it on each and every node
