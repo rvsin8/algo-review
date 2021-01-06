0715793f21ad3b287cf6b96064020307aaad26c8
rvsin8 <rsingh0815@gmail.com>
2021-01-06 17:34:55 -0500

    longest peak

--- Desktop/algoexpert/medium/longest_peak.js
+++ Desktop/algoexpert/medium/longest_peak.js
@@ -16,5 +16,14 @@
 //4 is because it is greater than both 3 and 0
 //10 is because it is greater than 0 and 6
 //the rest are not
-//now we have to compare the peak lengths, from both of the peaks lets traverse to the left and see how long we can go without strictly decreasing
+//now we have to compare the peak lengths, from both of the peaks lets traverse to the left and right and see how long we can go without strictly decreasing
+//once the num stop decreasing we found the edges
+//[3,4,0] vs [0,10,6,5,-1-3], length 3 vs 6
 
+//time complexity
+//with no peaks its constant time
+//with peaks its linear time O(N) bc if you have an array full of peaks - you stop at the points where the triangles meet
+//you will never visit more than N nodes/elements in an array again
+
+//space complexity
+//
