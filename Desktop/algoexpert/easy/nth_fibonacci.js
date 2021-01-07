385c14de9b4e5c6b36b2eab46241d3f9c0a1a5c9
rvsin8 <rsingh0815@gmail.com>
2021-01-06 19:47:22 -0500

    nth fib

--- Desktop/algoexpert/easy/nth_fibonacci.js
+++ Desktop/algoexpert/easy/nth_fibonacci.js
@@ -2,3 +2,7 @@
 //Recursion
 
 //notes
+//we are looking for the nth fibonacci number ion a sequence
+//fib sequence is a famous seq of num in math
+//first 2 num are 0 and 1, every num after can be found by adding the two previous nums
+//math formula is fib(n) = fib(n-1) + fib(n-2) for n > 2
