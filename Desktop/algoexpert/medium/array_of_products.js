61be17b995a5d4dda253398cdff32c8cc5eb5152
rvsin8 <rsingh0815@gmail.com>
2021-01-08 15:29:37 -0500

    array of products

--- Desktop/algoexpert/medium/array_of_products.js
+++ Desktop/algoexpert/medium/array_of_products.js
@@ -5,3 +5,5 @@
 //int can be positive 0, pos, neg and even dups
 //the input and output have to be the same length 
 //idx of the output cannot be the sum of that same idx of the input
+//[5,1,4,2] --> [8,40,10,20] 8 --> 1 * 4 *2
+//40 --> 5 * 4 * 2 and so on
