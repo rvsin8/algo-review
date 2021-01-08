2c5125e02515b74d3831b9dba71e73bfc8645611
rvsin8 <rsingh0815@gmail.com>
2021-01-08 15:26:38 -0500

    array of products

---
+++ Desktop/algoexpert/medium/array_of_products.js
@@ -0,0 +1,7 @@
+//Array of Products
+//Arrays
+
+//our array will contain int > 2 so it will have at-least 3
+//int can be positive 0, pos, neg and even dups
+//the input and output have to be the same length 
+//idx of the output cannot be the sum of that same idx of the input
