942e081ab6bd038a0f056ab56613e973abc0f7c9
rvsin8 <rsingh0815@gmail.com>
2021-01-08 19:42:29 -0500

    first duplicate value

--- Desktop/algoexpert/medium/first_duplicate_vale.js
+++ Desktop/algoexpert/medium/first_duplicate_vale.js
@@ -20,5 +20,16 @@
 //return 5
 
 //time complexity
+//we traverse through it once left to right
 
 //space complexity
+//we are not storing anything 
+
+function firstDuplicateValue(array) {
+    for (const value of array) {
+        const absValue = Math.abs(value);
+        if (array[absValue - 1] < 0) return absValue;
+        array[absValue - 1] *= -1;
+    }
+    return -1
+}
