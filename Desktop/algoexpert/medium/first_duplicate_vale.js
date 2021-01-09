64eb5482ec684efa339e491be7a23398e4d7d298
rvsin8 <rsingh0815@gmail.com>
2021-01-08 19:12:35 -0500

    array of products

---
+++ Desktop/algoexpert/medium/first_duplicate_vale.js
@@ -0,0 +1,13 @@
+//First Duplicate Value
+//Arrays
+
+//my understanding
+//where the values are btw 1 <= array[i] <= 7 (whatever is the length of the array)
+//we want to find the first duplicate values
+//[2,1,5,3,3,2,4] //3 bc the dups occurs before 2
+//we want the second occurrence with the minimum idx
+//if we have no dups then return -1
+
+//algorithm
+//we can do it in one traversal and constant space complexity 
+//
