ef1a35ce9719a5a1c60125f648d9e78525c5484a
rvsin8 <rsingh0815@gmail.com>
2021-01-11 15:03:31 -0500

    product sum

---
+++ Desktop/algoexpert/easy/product_sum.js
@@ -0,0 +1,12 @@
+//Product Sum
+//Recursion
+
+//my notes
+//we have a special array and we need to find the product of it
+//a special array that consists of numbers or integers or special arrays
+//if a special arrays can contain other special arrays then we know its recursions
+//product sum is the sum of all the elements 
+//when you get to a special array you multiple those ele plus their depth in the special array
+//initial array depth is being multiplied by 1
+//second depth array will be multiplied by 2
+//third depth array will be multiplied by 3 
