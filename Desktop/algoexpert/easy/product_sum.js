5299a2d86ae84045b55e4f643983bcdd4cf4dc35
rvsin8 <rsingh0815@gmail.com>
2021-01-11 15:11:05 -0500

    product sum

--- Desktop/algoexpert/easy/product_sum.js
+++ Desktop/algoexpert/easy/product_sum.js
@@ -10,3 +10,10 @@
 //initial array depth is being multiplied by 1
 //second depth array will be multiplied by 2
 //third depth array will be multiplied by 3 
+
+//[5,2,[7,-1],3,[6,[-13,8],4]] --> 12
+//initialize sum to 0 and just start iterating and add the ele up
+//sum =5, sum = 5 + 2 = 7
+//now we git a special array, here we apply recursive approach
+//if we hit an array --> add to the sum the product sum to the array recursively 
+//the multiplier increases as we go deeper in the array
