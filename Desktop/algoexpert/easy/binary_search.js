9653d42013c7565bf8e7bd1b1595aa0ce3901fa7
rvsin8 <rsingh0815@gmail.com>
2021-01-12 18:17:07 -0500

    product sum

---
+++ Desktop/algoexpert/easy/binary_search.js
@@ -0,0 +1,11 @@
+//Binary Search
+//Searching
+
+//my notes
+//[0,1,21,33,45,45,61,71,72,73], 33
+//in binary search the list and array need to be sorted
+//if you have a sorted array in an interview then think about binary search if you can use it
+//you going to want to find the middle num in the array first
+//based on the middle you'll either look through the first half or the second half of the array
+//you will have two pointer - left / right
+
