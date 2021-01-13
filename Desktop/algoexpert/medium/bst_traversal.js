514cc4d53596386e076cca149304f91b25f4a916
rvsin8 <rsingh0815@gmail.com>
2021-01-13 13:37:34 -0500

    bst traversal

--- Desktop/algoexpert/medium/bst_traversal.js
+++ Desktop/algoexpert/medium/bst_traversal.js
@@ -1,3 +1,8 @@
 //BST Traversal 
 //Binary Search Trees
 
+//my notes
+//in-order: [1,2,5,5,10,15,22]
+//pre-order: [10,5,2,1,5,15,22]
+//post-order: [1,2,5,5,22,15,10]
+//in-order traversal - wants you to traverse the bst in order, as in ascending order
