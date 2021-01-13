9e3fb0e6b71df99b32cfcfe23920e57b945ae02e
rvsin8 <rsingh0815@gmail.com>
2021-01-13 13:48:53 -0500

    bst traversal

--- Desktop/algoexpert/medium/bst_traversal.js
+++ Desktop/algoexpert/medium/bst_traversal.js
@@ -6,5 +6,13 @@
 //pre-order: [10,5,2,1,5,15,22]
 //post-order: [1,2,5,5,22,15,10]
 //in-order traversal - wants you to traverse the bst in order, as in ascending order, we look at the left node first, then the current node and then the right node. 
-//inorder traverse(left); array.append(current val); inorder traverse(right)
-//pre-order:
+//inorder traverse(left); array.append(current val); inorder traverse(right) //core logic for these traversals
+//for pre/post order we will change these three steps order ^
+//for pre array append will be first
+//for post it will be last array append
+
+//time complexity 
+//O(n) bc we need to traverse every single node
+
+//space complexity
+//O(n) bc we are creating an array of length n where we are storing all these values
