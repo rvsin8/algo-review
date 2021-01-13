7e7fe270139a8cb29774db59df1462402dc4ad15
rvsin8 <rsingh0815@gmail.com>
2021-01-12 19:08:35 -0500

    validate bst

--- Desktop/algoexpert/medium/validate_bst.js
+++ Desktop/algoexpert/medium/validate_bst.js
@@ -6,3 +6,8 @@
 //the data structure contains nodes or node values
 //the question wants to know if the data structure is a bst
 //a bst is a ds in which every node is strictly greater than its left values and a value less/equal of its right nodes
+
+
+//divide and conquer approach
+//go down each node and validate their left / right trees
+//each node has a min / max value, for example for 5, min value would be 5 and 10 is the greatest val bc its the root node
