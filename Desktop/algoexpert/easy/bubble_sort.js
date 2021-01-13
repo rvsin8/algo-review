8bb65d97ffeef91255f6e381410de6205401efab
rvsin8 <rsingh0815@gmail.com>
2021-01-13 14:47:55 -0500

    bubble sort

--- Desktop/algoexpert/easy/bubble_sort.js
+++ Desktop/algoexpert/easy/bubble_sort.js
@@ -2,4 +2,7 @@
 //Sorting
 
 //my notes
-//
+//important algorithm in terms of complexity analysis and helps with the other sort algorithms 
+//the goal is to sort a list of numbers in ascending order
+//we will iterate the array multiple times and perform swaps
+//we will check the current num and if its smaller than or bigger than the next number
