26375172900ecf9597ea01950cd608dd1ead8949
rvsin8 <rsingh0815@gmail.com>
2021-01-12 19:16:09 -0500

    validate bst

--- Desktop/algoexpert/medium/validate_bst.js
+++ Desktop/algoexpert/medium/validate_bst.js
@@ -11,3 +11,9 @@
 //divide and conquer approach
 //go down each node and validate their left / right trees
 //each node has a min / max value, for example for 5, min value would be 5 and 10 is the greatest val bc its the root node
+//5 <= node.value < 10
+//we might need a helper function that takes the parent node
+//when we call it on the left side of the node we will change the min val to 5 and max to 10 which was the parent node 
+//we will validate every subtree using this algo
+// -inf <= node.value < 10
+
