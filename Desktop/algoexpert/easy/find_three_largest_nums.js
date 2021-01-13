b2d7068fbec92c84de25352c79f591092ca06a06
rvsin8 <rsingh0815@gmail.com>
2021-01-12 20:46:13 -0500

    find three largest nums

---
+++ Desktop/algoexpert/easy/find_three_largest_nums.js
@@ -0,0 +1,7 @@
+//Find Three Largest Numbers
+//Searching 
+
+//my notes
+//you are given an array of integers, may not be sorted, and our task is the three largest integers
+//[141,1,17,-7,-17,-27,18,541,8,7,7] --> [18, 141, 541]
+//we want to keep track and store the 3 largest num and keep traversing 
