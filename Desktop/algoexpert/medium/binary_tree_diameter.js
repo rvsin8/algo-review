5340d46435d6b5ef372885830087f586e01cc13f
rvsin8 <rsingh0815@gmail.com>
2021-01-13 15:51:09 -0500

    binary tree diameter

---
+++ Desktop/algoexpert/medium/binary_tree_diameter.js
@@ -0,0 +1,8 @@
+//Binary Tree Diameter
+//Binary Trees
+
+//my notes
+//binary tree is a ds in which each branch has two nodes at most or no nodes
+//binary tree do not have to be ordered unlike bionary search tree
+//a diameter of a binary tree diameter which is the longest path in the tree.
+//the longest path does no have to go through the root
