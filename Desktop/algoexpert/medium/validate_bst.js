aa59b8f206144885fa117afea12e60881d819d93
rvsin8 <rsingh0815@gmail.com>
2021-01-12 19:05:44 -0500

    validate bst

---
+++ Desktop/algoexpert/medium/validate_bst.js
@@ -0,0 +1,8 @@
+//Validate BST
+//Binary Search Trees
+
+//my notes
+//this question will take in a data structure like a binary tree
+//the data structure contains nodes or node values
+//the question wants to know if the data structure is a bst
+//a bst is a ds in which every node is strictly greater than its left values and a value less/equal of its right nodes
