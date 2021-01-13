8daa29d313cd5ef8408efb5bf1fa10a444f86759
rvsin8 <rsingh0815@gmail.com>
2021-01-13 14:51:47 -0500

    bubble sort

--- Desktop/algoexpert/easy/bubble_sort.js
+++ Desktop/algoexpert/easy/bubble_sort.js
@@ -6,3 +6,13 @@
 //the goal is to sort a list of numbers in ascending order
 //we will iterate the array multiple times and perform swaps
 //we will check the current num and if its smaller than or bigger than the next number
+//if they are not in order, then we swap the nums in the array
+//keep doing this until everything is sorted
+//if there are no more swaps then we are done
+//[8,5,2,9,5,6,3]
+//[5,8,2,9,5,6,3]
+//[5,2,8,9,5,6,3]
+//[5,2,8,5,9,6,3]
+//[5,2,8,5,6,9,3]
+//[5,2,8,5,6,3,9] //we are done with ONE iteration but the entire array is not sorted
+
