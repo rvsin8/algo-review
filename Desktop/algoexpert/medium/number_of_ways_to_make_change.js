0a89f9257f00bdccf8d4abced42dd744a74ce5b2
rvsin8 <rsingh0815@gmail.com>
2021-01-14 18:04:14 -0500

    number of ways to make change

--- Desktop/algoexpert/medium/number_of_ways_to_make_change.js
+++ Desktop/algoexpert/medium/number_of_ways_to_make_change.js
@@ -35,5 +35,5 @@ function numberOfWaysToMakeChange(n, denoms) { //n is an integer val and so is d
             }
         }
     }
-    return ways[n];
+    return ways[n]; //return the ways of n
 }
