647317c697eb1ec8dff79f3acf4e7eca313fc8c2
rvsin8 <rsingh0815@gmail.com>
2021-01-14 19:58:18 -0500

    min number of coins for change

--- Desktop/algoexpert/medium/min_num_of_coins_for_change.js
+++ Desktop/algoexpert/medium/min_num_of_coins_for_change.js
@@ -1,4 +1,9 @@
 //Min Number of Coins for Change
 //Dynamic Programmingå
 
-
+//my notes
+//we get two inputs
+//one is an integer value of the amount of money
+//the other input is an array of integer values that represent coin denomination
+//$6 [1,2,4]
+//minimum amount of coins we need to reach $6
