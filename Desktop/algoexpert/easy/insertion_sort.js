6aabb7f55b560366db85648305613ab237be2f74
rvsin8 <rsingh0815@gmail.com>
2021-01-26 17:41:07 -0500

    insertion sort

--- Desktop/algoexpert/easy/insertion_sort.js
+++ Desktop/algoexpert/easy/insertion_sort.js
@@ -13,20 +13,20 @@
 //best case would be an already sorted list which would be O(N)
 
 function insertionSort(array) {
-    for (let i = 1; i < array.length; i++){
-        let j = i;
-        while (j > 0 && array[j] < array[j - 1]) {
-            swap(j, j-1, array);
-            j -= 1;
+    for (let i = 1; i < array.length; i++){// initialize for loop and start at the next one, we want to tentatively sort it at idx 11`
+        let j = i; //insert the num by having a sex idx at j equal to i
+        while (j > 0 && array[j] < array[j - 1]) { //while array j is greater than 0 and array j is less than array j - 1 -- out of order
+            swap(j, j-1, array); //so we swap j, j-1 and array
+            j -= 1; //and we decrement j and track the num we are trying to insert
         }
     }
-    return array;
+    return array; //return sorted array
 
 }
 
-function swap(i, j, array) {
+function swap(i, j, array) { //swap helper
     const temp = array[j];
     array[j] = array[i];
     array[i] = temp;
-    
+
 }
