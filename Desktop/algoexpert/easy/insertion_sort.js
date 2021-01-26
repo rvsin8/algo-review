65eac95137c01e9fce1ae8f90d02c5d717c3772f
rvsin8 <rsingh0815@gmail.com>
2021-01-26 17:35:47 -0500

    insertion sort

--- Desktop/algoexpert/easy/insertion_sort.js
+++ Desktop/algoexpert/easy/insertion_sort.js
@@ -11,3 +11,22 @@
 //time complexity 
 //O(n^2) - we have to do a bunch of comparisons and swaps 
 //best case would be an already sorted list which would be O(N)
+
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++){
+        let j = i;
+        while (j > 0 && array[j] < array[j - 1]) {
+            swap(j, j-1, array);
+            j -= 1;
+        }
+    }
+    return array;
+
+}
+
+function swap(i, j, array) {
+    const temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+    
+}
