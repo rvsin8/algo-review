34430cb357e9b114c9756b6bda7467f2fd72a385
rvsin8 <rsingh0815@gmail.com>
2021-01-26 19:46:08 -0500

    kadane's algorithm

--- Desktop/algoexpert/medium/kadanes_algorithm.js
+++ Desktop/algoexpert/medium/kadanes_algorithm.js
@@ -1,5 +1,5 @@
 //Kadane's Algorithm
-//Famous Algorithm
+//Famous Algorithm / Dynamic Programming
 
 //my understanding 
 //finding the subarray that produces the greatest sum from ele adjacent from each other
@@ -7,4 +7,4 @@
 //we just need the sum not the actual subarray that contains the largest sum
 //if we have negative numbers, it gets tricky - in some cases neg num well ruin potential candidates for greatest sums
 //in some cases neg numbers won't be so bad 
-//
+//take subarrays and find the answer for every single one that it ends with
