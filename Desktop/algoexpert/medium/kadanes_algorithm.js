02386f6f9f31a17235ecee484d4e6e4e620ee17c
rvsin8 <rsingh0815@gmail.com>
2021-01-26 19:36:20 -0500

    kadane's algorithm

--- Desktop/algoexpert/medium/kadanes_algorithm.js
+++ Desktop/algoexpert/medium/kadanes_algorithm.js
@@ -4,3 +4,7 @@
 //my understanding 
 //finding the subarray that produces the greatest sum from ele adjacent from each other
 //[3,5,-9,1,3,-2,3,4,7,2,-9,6,3,1,-5,4]
+//we just need the sum not the actual subarray that contains the largest sum
+//if we have negative numbers, it gets tricky - in some cases neg num well ruin potential candidates for greatest sums
+//in some cases neg numbers won't be so bad 
+//
