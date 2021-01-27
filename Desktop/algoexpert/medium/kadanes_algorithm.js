a927ba2bab10b9c516997493e360ba43041dd7da
rvsin8 <rsingh0815@gmail.com>
2021-01-27 15:22:19 -0500

    kadane's algorithm

--- Desktop/algoexpert/medium/kadanes_algorithm.js
+++ Desktop/algoexpert/medium/kadanes_algorithm.js
@@ -7,4 +7,10 @@
 //we just need the sum not the actual subarray that contains the largest sum
 //if we have negative numbers, it gets tricky - in some cases neg num well ruin potential candidates for greatest sums
 //in some cases neg numbers won't be so bad 
-//take subarrays and find the answer for every single one that it ends with
+//take subarrays and find the answer for every single one that it ends with like 3 or 5 or etc
+//if it ends with 3 the greatest sum is obviously 3
+//with array [3,5] the answer is 8
+//with 3,5 and -9 [3,5,-9] is -1
+//we can either sum up 3 5 -9 1 which is 0 or just 1 and here 1 is greater than 0 --> so we choose 1
+//max Sum = -1 + 1 or just 1 (we will do this at each idx to see if we want to recount from that idx)
+//
