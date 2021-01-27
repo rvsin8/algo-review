ed4ac29d7feb318e4f7e17857678f5dbb2a9a640
rvsin8 <rsingh0815@gmail.com>
2021-01-26 19:13:30 -0500

    leveshtein distance

--- Desktop/algoexpert/medium/levenshtein_distance.js
+++ Desktop/algoexpert/medium/levenshtein_distance.js
@@ -26,3 +26,35 @@
 
 //space complexity
 //O(nm) we are only storing in that 2D array and nothing else
+//we may also just use the last two rows --> making it O(min(n,m))
+
+function levenshteinDistance(str1, str2){
+    const small = str1.length < str2.length ? str1 : str2;
+    const big = str1.length >= str2.length ? str1 : str2;
+    const evenEdits = [];
+    const oddEdits = new Array(small.length + 1);
+    for (let j = 0; j < small.length + 1; j++){
+        evenEdits.push(j);
+    }
+
+    for(let i = 1; i < big.length + 1; i++){
+        let currentEdits, previousEdits;
+        if (i % 2 === 1){
+            currentEdits = oddEdits;
+            previousEdits = evenEdits;
+        } else {
+            currentEdits = evenEdits;
+            previousEdits = oddEdits;
+        }
+    }
+
+    currentEdits[0] = i;
+    for (let j = 1; j < small.length + 1; j++){
+        if (big[i - 1] === small[j - 1]) {
+            currentEdits[j] = previousEdits[j - 1];
+        } else {
+            currentEdits[j] = 1 + Math.min(previousEdits[j - 1], previousEdits[j], currentEdits[j - 1]);
+        }
+    }
+    return big.length % 2 === 0 ? eventEdits[small.length] : oddEdits[small.length];
+}
