9ae08ebd9a627e7650a3fbe10a041ac54c8010fd
rvsin8 <rsingh0815@gmail.com>
2021-02-03 15:47:42 -0500

    youngest common ancestor

--- Desktop/algoexpert/medium/youngest_common_ancestor.js
+++ Desktop/algoexpert/medium/youngest_common_ancestor.js
@@ -3,4 +3,5 @@
 
 //my understanding 
 //gives us three inputs - the first one is the top ancestor and the other two inputs are two descendants of that ancestor
-//
+//we are told the three inputs are part of one class that has a ancestor property
+//we want to bring the nodes at the same level and then find the youngest common ancestor
