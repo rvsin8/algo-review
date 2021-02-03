fc6d8d7e33fab1090d75ff78e3663ae891ea275a
rvsin8 <rsingh0815@gmail.com>
2021-02-03 16:37:02 -0500

    selection sort

--- Desktop/algoexpert/easy/selection_sort.js
+++ Desktop/algoexpert/easy/selection_sort.js
@@ -1,3 +1,8 @@
 //Selection Sort
 //Sorting
 
+//you are given a list of numbers and  and ordered and unordered list
+//[8,5,2,9,5,6,3]
+//we have a pointer where we traverse and array to find a smaller num than 8 aka 5
+//we keep doing bc 2 is less than 5
+//2 is the smallest so we did not find any smaller num in the array so we swap the 2 with 8 so it can be the first ele in the array
