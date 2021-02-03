257574258df3567414f22f4af6b4d735f5d9a326
rvsin8 <rsingh0815@gmail.com>
2021-02-03 13:07:46 -0500

    breadth first search

--- Desktop/algoexpert/medium/breadth-first_search.js
+++ Desktop/algoexpert/medium/breadth-first_search.js
@@ -1,3 +1,10 @@
 //Breadth-First Search
 //Graphs 
 
+//my understanding
+//harder than depth-first search to implement
+//must know
+//we are traversing a graph (this ex has a tree-like data structure with nodes in which each node has a name which is a letter in the alphabet and may have children nodes as well)
+//write this graph in a breadth-depth first way, grab all the nodes and put them in an array and return that array
+//the way bdf works, we want to traverse the graph level by level from the top level aka root level than go down levels
+
