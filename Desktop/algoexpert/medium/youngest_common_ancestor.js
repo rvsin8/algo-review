353caf72531a0336460d510efce752cde7144b70
rvsin8 <rsingh0815@gmail.com>
2021-02-03 16:08:42 -0500

    youngest common ancestor

--- Desktop/algoexpert/medium/youngest_common_ancestor.js
+++ Desktop/algoexpert/medium/youngest_common_ancestor.js
@@ -5,3 +5,10 @@
 //gives us three inputs - the first one is the top ancestor and the other two inputs are two descendants of that ancestor
 //we are told the three inputs are part of one class that has a ancestor property
 //we want to bring the nodes at the same level and then find the youngest common ancestor
+//yo what the fuck is going on
+
+//time complexity
+//O(d) where d is the depth
+
+//space complexity 
+//O(1) bc we can write this algo iteratively with a few while loops and nothing needs to be stored
