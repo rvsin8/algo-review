d72a223496e987e964859c907f67a84a2c872a8d
rvsin8 <rsingh0815@gmail.com>
2021-02-03 15:39:47 -0500

    youngest common ancestor

---
+++ Desktop/algoexpert/medium/youngest_common_ancestor.js
@@ -0,0 +1,6 @@
+//Youngest Common Ancestor
+//Graphs
+
+//my understanding 
+//gives us three inputs - the first one is the top ancestor and the other two inputs are two descendants of that ancestor
+//
