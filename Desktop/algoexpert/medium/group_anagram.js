ca1c80e74d16ad63ec856c6189c0b912d57ca6dc
rvsin8 <rsingh0815@gmail.com>
2021-02-04 17:14:22 -0500

    group anagrams

--- Desktop/algoexpert/medium/group_anagram.js
+++ Desktop/algoexpert/medium/group_anagram.js
@@ -4,3 +4,6 @@
 //my understanding 
 //gives us a list of words / strings and write a function that will return a list of other lists of aanagrams 
 //anagrams are words that are written in the same letters but written differently
+//you can put each word in alphabetical order and match the strings that are identical
+//[yo, act, flop, tac, cat, oy, olfp] --> [oy, act, flop, act, act, oy, flop] --> [act, act, act, flop, flop, oy, oy] --> [[yo,oy], [flop, olfp], [act, tac, cat]]
+//
