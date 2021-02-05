d173fa1bcd79263322257168430c0b3dce013e66
rvsin8 <rsingh0815@gmail.com>
2021-02-05 07:51:20 -0500

    longest palindromic substring

--- Desktop/algoexpert/medium/longest_palindromic_substring.js
+++ Desktop/algoexpert/medium/longest_palindromic_substring.js
@@ -2,4 +2,7 @@
 //Strings
 
 //my understanding 
+//gives us a string of characters, write a function that will return the largest substring in the string that is a palindrome
+//abaxyzzyxf --> xyzzyx
+//get all possible substrings and for each one check if it a palindrome  is one way to solve it but has bad time complexity 
 //
