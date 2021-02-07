89a9241f2bcee09b32520eaac05fafd7333711fd
rvsin8 <rsingh0815@gmail.com>
2021-02-06 21:39:10 -0500

    remove kth node from end

--- Desktop/algoexpert/medium/remove_nth_node_from_end.js
+++ Desktop/algoexpert/medium/remove_nth_node_from_end.js
@@ -7,3 +7,25 @@
 //time complexity
 
 //space complexity
+
+function removeKthNodeFromEnd(head, k) {
+    let counter = 1;
+    let first = head;
+    let second = head;
+    while (counter <= k) {
+        second = second.next;
+        counter++;
+    }
+
+    if (second === null) {
+        head.value = head.next.value;
+        head.next = head.next.next;
+        return;
+    }
+    
+    while (second.unit !== null) {
+        second = second.next;
+        first = first.next;
+    }
+    first.next = first.next.nest;
+}
