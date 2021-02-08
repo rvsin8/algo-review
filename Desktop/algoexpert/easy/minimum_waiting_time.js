caa186dd0ff3105d85ba1baff67b48cf7e9eac1a
rvsin8 <rsingh0815@gmail.com>
2021-02-08 00:14:01 -0500

    minimum waiting time

--- Desktop/algoexpert/easy/minimum_waiting_time.js
+++ Desktop/algoexpert/easy/minimum_waiting_time.js
@@ -8,6 +8,9 @@
 //[3,2,1,2,6]
 //[6,1,2,2,3] - query1 = 0 , query2 = 6, query3 = 6 + 1 = 7, query4 = 6 + 1 + 2 = 9, query5 = 11 --> 11 + 9 + 6 + 7 = 33 not the answer is 17
 //find a better order to find minimum waiting time
+//the minimum length we are given is 1
+//we should not execute the largest query first
+//if we put the list in sorted order then it would be [1,2,2,3,6] --> 17 which is the answer
 
 //time complexity 
 //O(nlogn)
