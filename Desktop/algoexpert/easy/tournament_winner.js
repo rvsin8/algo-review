4102f07d17bb399cbcbc34dd4e96225bae0bab1c
rvsin8 <rsingh0815@gmail.com>
2021-02-09 16:18:41 -0500

    tournament winner

--- Desktop/algoexpert/easy/tournament_winner.js
+++ Desktop/algoexpert/easy/tournament_winner.js
@@ -14,5 +14,14 @@ const HOME_TEAM_WON = 1;
 function tournamentWinner(competitions, results) {
     let currentBestTeam = '';
     const scores = {[currentBestTeam]: 0};
-    
+
+    for (let idx = 0; idx < competitions.length; idx++) {
+        const result = results[idx];
+        const [homeTeam, awayTeam] = competitons[idx];
+
+        const winningTeam = result === HOME_TEAM_WON ? homeTeam : awayTeam;
+
+        updateScores(winningTeam, 3, scores);
+    }
+
 }
