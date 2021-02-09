0682cd6e3d311a18ffc630f0ecb3e70d66aa338d
rvsin8 <rsingh0815@gmail.com>
2021-02-09 07:41:26 -0500

    balanced brackets

--- Desktop/algoexpert/medium/balanced_brackets.js
+++ Desktop/algoexpert/medium/balanced_brackets.js
@@ -14,4 +14,19 @@ function balancedBrackets(string) {
     const closingBrackets = ')]}';
     const matchingBrackets = {')' : '(', ']' : '[', '}' : '{'};
     const stack = [];
+    for (const char of strings) {
+        if (openingBrackets.includes(char)) {
+            stack.push(char);
+        } else if (closingBrackets.includes(char)) {
+            if (stack.length == 0) {
+                return false;
+            }
+            if (stack[stack.length - 1] === matchingBrackets[char]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
 }
