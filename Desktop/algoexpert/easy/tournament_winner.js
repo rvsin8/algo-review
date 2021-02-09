253708b4c302a82361e78b0af9a4cb578188423a
rvsin8 <rsingh0815@gmail.com>
2021-02-09 17:39:15 -0500

    tournament winner

--- Desktop/algoexpert/easy/tournament_winner.js
+++ Desktop/algoexpert/easy/tournament_winner.js
@@ -42,22 +42,22 @@ function tournamentWinner(competitions, results) { //takes in two parameters
 
     for (let idx = 0; idx < competitions.length; idx++) { //access to the value and ele in our competition array with its indices 
         const result = results[idx]; //our corresponding index
-        const [homeTeam, awayTeam] = competitons[idx];
+        const [homeTeam, awayTeam] = competitons[idx]; //splits it into 2 components
 
-        const winningTeam = result === HOME_TEAM_WON ? homeTeam : awayTeam;
+        const winningTeam = result === HOME_TEAM_WON ? homeTeam : awayTeam; //the winning team is equal to the home team if the result if equal to the home team won else it is the away team
 
-        updateScores(winningTeam, 3, scores);
+        updateScores(winningTeam, 3, scores); //update the score with the winning, three points and the total score
 
-        if (scores[winningTeam] > scores[currentBestTeam]) {
-            currentBesTeam = winningTeam;
+        if (scores[winningTeam] > scores[currentBestTeam]) { //if scores of the winning team is better than the score of the current best team
+            currentBesTeam = winningTeam; //we update that winning team to the current best team
         }
     }
     return currentBestTeam;
 
 }
 
-function updateScores(team, points, scores) {
-    if (!(team in scores)) scores[team] = 0;
+function updateScores(team, points, scores) { 
+    if (!(team in scores)) scores[team] = 0; //if the key does not exist in the ds, we need to add it then to the ds
 
-    scores[team] += points;
+    scores[team] += points; //add the points to the team given here
 }
