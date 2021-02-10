ebb758529614407fe96f2cfd8c1a1ae6fc0e936c
rvsin8 <rsingh0815@gmail.com>
2021-02-09 19:15:40 -0500

    class photos

--- Desktop/algoexpert/easy/class_photos.js
+++ Desktop/algoexpert/easy/class_photos.js
@@ -10,3 +10,10 @@
 //space complexity
 //O(1)
 
+function classPhotos(redShirtHeights, blueShirtHeights) {
+    redShirtHeights.sort((a,b) => b - a);
+    blueShirtHeights.sort((a,b) => b - a);
+
+    const shirtColorInFirstRow = redShirtHeights[0] < blueShirtHeights[0] ? 'RED' : 'BLUE';
+    
+}
