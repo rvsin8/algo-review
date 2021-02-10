6ad5811a755639fba287a7ae550856bc39c56a63
rvsin8 <rsingh0815@gmail.com>
2021-02-09 19:17:01 -0500

    class photos

--- Desktop/algoexpert/easy/class_photos.js
+++ Desktop/algoexpert/easy/class_photos.js
@@ -15,5 +15,15 @@ function classPhotos(redShirtHeights, blueShirtHeights) {
     blueShirtHeights.sort((a,b) => b - a);
 
     const shirtColorInFirstRow = redShirtHeights[0] < blueShirtHeights[0] ? 'RED' : 'BLUE';
-    
+    for (let idx = 0; idx < redShirtHeights.length; idx++) {
+        const redShirtHeights = redShirtHeights[idx];
+        const blueShirtHeights = blueShirtHeights[idx];
+
+        if (shirtColorInFirstRow === 'RED') {
+            if (redShirtHeights >= blueShirtHeights) return false;
+
+        } else if (blueShirtHeights >= redShirtHeights) return false;
+    }
+
+    return true;
 }
