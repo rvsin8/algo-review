e08ea9339d348275120b503443a4fd9fba945cb1
rvsin8 <rsingh0815@gmail.com>
2021-02-10 06:57:16 -0500

    non-constructible change

--- Desktop/algoexpert/easy/non-constructible_change.js
+++ Desktop/algoexpert/easy/non-constructible_change.js
@@ -2,6 +2,9 @@
 //Arrays
 
 //my understanding 
+//given an array of positive integers and qwill represent the values of coins and they do not have to be unique
+//1 is 1 cent, 1007 is 1007 cent
+//look at all these coins and return the minimum amount of change we CANNOT make
 
 
 //time complexity 
@@ -10,3 +13,15 @@
 //space complexity 
 //O(1)
 
+function nonConstructibleCHnage(coins) {
+    coins.sort((a,b) => a - b);
+
+    let currentChangeCreated = 0;
+    for (const coin of coins) {
+        if (coin > currentChangeCreated + 1) return currentChangeCreated + 1;
+
+        currentChangeCreated += coin;
+    }
+
+    return currentChangeCreated + 1;
+}
