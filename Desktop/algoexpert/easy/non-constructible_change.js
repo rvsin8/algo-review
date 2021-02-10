d4a68be27256411fede45af6da3d6c9efd169a84
rvsin8 <rsingh0815@gmail.com>
2021-02-10 06:59:02 -0500

    non-constructible change

--- Desktop/algoexpert/easy/non-constructible_change.js
+++ Desktop/algoexpert/easy/non-constructible_change.js
@@ -5,6 +5,13 @@
 //given an array of positive integers and qwill represent the values of coins and they do not have to be unique
 //1 is 1 cent, 1007 is 1007 cent
 //look at all these coins and return the minimum amount of change we CANNOT make
+//[7] --> 1
+//[5,7,1,1,2,3,22] --> 20
+//[1,2,5] --> 4
+
+//my solution
+//brute force approach is not optimal -- just loop through all positive integers until we hit one we cannot make
+
 
 
 //time complexity 
