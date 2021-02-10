041a40ee544dc38e6a6e11732cc160ee0a782060
rvsin8 <rsingh0815@gmail.com>
2021-02-10 07:07:23 -0500

    non-constructible change

--- Desktop/algoexpert/easy/non-constructible_change.js
+++ Desktop/algoexpert/easy/non-constructible_change.js
@@ -14,6 +14,15 @@
 //optimal solution 
 //first sort the input array - [1,1,2,3,5,7,22]; change = k
 //1..k is all the change we can make
+//first ele we hit is 1 meaning we can make 1 cent of change
+//next ele is also a 1 which means we can make 2 cents of change 
+//next is 2 and we can make 3 and 4 change
+//next is 3, we can make 4, 5 and 6 cent of change
+//then is 5, we can make from 1-12 cents
+//then is 7, 1-18 yes 
+//finally 22 --> we cannot make 20 cents
+//change = previous coin + 1 //22 is greater thn our change + 1 and thats the first time it occurs in the array
+
 
 
 //time complexity 
