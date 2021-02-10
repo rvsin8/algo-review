d77f84cd52546a9e9b4f288512102d61246992aa
rvsin8 <rsingh0815@gmail.com>
2021-02-09 19:30:57 -0500

    class photos

--- Desktop/algoexpert/easy/class_photos.js
+++ Desktop/algoexpert/easy/class_photos.js
@@ -2,6 +2,14 @@
 //Greedy Algorithms 
 
 //my understanding
+//given two input arrays - height of students wearing a blue shirt and height of students wearing a red shirt
+//arrays will be the same length, so same ppl wearing blue or red shirts
+//can we take a pic of students with the following constraints 
+//all the students wearing red shirts must be in the same row
+//all the students wearing blue shirts must be in the same row
+//must have exactly two rows
+//everyone in the front row must be shorter than those in the back row
+//return true if we are able to take a pic accordingly 
 
 
 //time complexity 
