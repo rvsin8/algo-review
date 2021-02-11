0e7952720e6b2df95ffd53e88452be2837a23794
rvsin8 <rsingh0815@gmail.com>
2021-02-11 04:34:00 -0500

    search in sorted matrix

--- Desktop/algoexpert/medium/search_in_sorted_matrix.js
+++ Desktop/algoexpert/medium/search_in_sorted_matrix.js
@@ -5,8 +5,14 @@
 //look for a number in a 2D array aka a matrix
 //matrix is sorted, every row and col are sorted in ascending order
 //we have a target number and if it is in the matrix then we want the indices at which that matrix is placed
-
-
+//a sorted matrix can tell us ? we can start from the middle of the matrix which is 33 and it is smaller than our target number which is 44
+//this helps us eliminate all the numbers above and to the left of 33 since they are smaller than 44 as well
+//we have to search to the right and to the bottom of 33
+//it gets rid of the top left corner
+//but lets try another way
+//start at the top right which is a 1000 and a 1000 is > 44
+//it tells us that 44 cannot be in the column bc 1000 is on top so we get rid of an entire column
+//move to the left to 15 which is < 44
 
 //time complexity 
 //O(n + m)
