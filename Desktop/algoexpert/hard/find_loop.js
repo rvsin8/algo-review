1bbb4fe4887410dac50abf0fd815add924a0587a
rvsin8 <rsingh0815@gmail.com>
2021-02-11 16:14:09 -0500

    find loop

--- Desktop/algoexpert/hard/find_loop.js
+++ Desktop/algoexpert/hard/find_loop.js
@@ -2,6 +2,17 @@
 //Linked Lists
 
 //my understanding
+//finding a loop in a linked list
+//basically you are given a singly linked list - every node ot pointed to ONE node
+//we want to find the node in which our loop is initiated 
+//traverse the entire linked list, add each node to the hash table and see what node is already in the hash table that is hit again
+//not the best solution bc it requires extra space
+
+//solution with no space
+//technique - traverse the list with two pointers
+//you start with the pointer in the first node in the list and a second pointer there as well
+//second pointer will go through every other node while the first pointer traverses through each one
+
 
 
 //time complexity 
