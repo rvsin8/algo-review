2b58bfc1abdda25b4c52b2111f0d27bcbafd1cdf
rvsin8 <rsingh0815@gmail.com>
2021-02-11 04:17:20 -0500

    search in sorted matrix

--- Desktop/algoexpert/medium/search_in_sorted_matrix.js
+++ Desktop/algoexpert/medium/search_in_sorted_matrix.js
@@ -10,3 +10,10 @@
 //space complexity
 //O(1)
 
+function searchInSortedMatrix(matrix, target) {
+    let row = 0;
+    let col = matrix[0].length - 1;
+    while (row < matrix.length && col >= 0) {
+        
+    }
+}
