f86ac7c90b44c37b55a8a00b9daa9d2449a93002
rvsin8 <rsingh0815@gmail.com>
2021-02-11 20:43:12 -0500

    sort K-Sorted Array

--- Desktop/algoexpert/hard/sort_k-sorted_array.js
+++ Desktop/algoexpert/hard/sort_k-sorted_array.js
@@ -1,3 +1,31 @@
 //Sort K-Sorted Array
 //Heaps
 
+//my understanding
+
+
+//time complexity
+
+
+//space complexity
+
+function sortKSortedArray(array, k) {
+    const minHeapWithKElements = new MinHeap(array.slice(0, Math.min(k+1, array.length)));
+
+    let nextIndexToInsertElement = 0;
+    for (let idx = k + 1; idx < array.length; idx++) {
+        const minElement = minHeapWithKElements.remove();
+        array[nextIndexToInsertElement] = minElement;
+        nextIndexToInsertElement++;
+
+        const currentElement = array[idx];
+        minHeapWithKElements.insert(currentElement);
+    }
+
+    while (@minHeapWithKElements.isEmpty()){
+        const minElement = minHeapWithKElements.remove();
+        array[nextIndexToInsertElement] = minElement;
+        nextIndexToInsertElement++;
+    }
+    return array;
+ }
