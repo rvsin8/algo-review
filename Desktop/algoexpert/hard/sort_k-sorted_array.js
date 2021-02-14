25dd5aae0ce130ba99fd3ece3f81c1f0451d69fe
rvsin8 <rsingh0815@gmail.com>
2021-02-14 18:47:26 -0500

    sort k-sorted array

--- Desktop/algoexpert/hard/sort_k-sorted_array.js
+++ Desktop/algoexpert/hard/sort_k-sorted_array.js
@@ -4,7 +4,13 @@
 //my understanding
 //we are given two inpurs - the first input is a partially sorted array and the second input will be the integer k and this value will always be greater than or equal to 0
 //a k sorted array is defined as an array where the integers are k sorted from their sorted positions
+//[3,2,1,5,4,7,6,5] 
+//we can look at 1 being in idx 2 which is three positions away from where it should be at position 0
+//we can look at 4 which is one position away
+//no integer is more than 3 positions away from its sorted idx
 
+//my solution
+//
 
 //time complexity
 
