55e42f7341549f176c20a6d255d8a8214bbb575e
rvsin8 <rsingh0815@gmail.com>
2021-02-14 20:05:04 -0500

    sort k-sorted array

--- Desktop/algoexpert/hard/sort_k-sorted_array.js
+++ Desktop/algoexpert/hard/sort_k-sorted_array.js
@@ -41,7 +41,7 @@
 //O(k) we have k + 1 or k elements in our heap
 
 function sortKSortedArray(array, k) {
-    const minHeapWithKElements = new MinHeap(array.slice(0, Math.min(k+1, array.length)));
+    const minHeapWithKElements = new MinHeap(array.slice(0, Math.min(k+1, array.length))); //this is how we initialize our min heap
 
     let nextIndexToInsertElement = 0;
     for (let idx = k + 1; idx < array.length; idx++) {
@@ -62,11 +62,11 @@ function sortKSortedArray(array, k) {
  }
 
 class MinHeap {
-    constructor(array) {
+    constructor(array) { 
         this.heap = this.buildHeap(array);
     }
 
-    isEmpty() {
+    isEmpty() { //we use this as well
         return this.heap.length === 0;
     }
 
@@ -81,7 +81,52 @@ class MinHeap {
     siftDown(currentIdx, endIdx, heap) {
         let childOneIdx = currentIdx * 2 + 1;
         while (childOneIdx <= endIdx) {
-            const childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentI
+            const childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
+            let idxToSwap;
+            if (childTwoIdx !== -1 && heap[childTwoIdx] < heap[childOneIdx]) {
+                idxToSwap = childTwoIdx;
+            } else {
+                idxToSwap = childOneIdx;
+            }
+            if (heap[idxToSwap] < heap[currentIdx]) {
+                this.swap(currentIdx, idxToSwap, heap);
+                currentIdx = idxToSwap;
+                childOneIdx = currentIdx * 2 + 1;
+            } else {
+                return;
+            }
         }
     }
+
+    siftUp(currentIdx, heap) {
+        let parentIdx = Math.floor((currentIdx - 1) / 2);
+        while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
+            this.swap(currentIdx, parentIdx, heap);
+            currentIdx = parentIdx;
+            parentIdx = Math.floor((currentIdx - 1) / 2);
+        }
+    }
+
+    peek() {
+        return this.heap[0];
+    }
+
+    remove() { //for this problem
+        this.swap(0, this.heap.length - 1; this.heap);
+        const valueToRemove = this.heap.pop();
+        this.siftDown(0, this.heap.length - 1, this.heap);
+        return valueToRemove;
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.siftUp(this.heap.length - 1, this.heap);
+    }
+
+    swap(i, j, heap) { //for this problem 
+        const temp = heap[j];
+        heap[j] = heap[i];
+        heap[i] = temp;
+    }
+
 }
