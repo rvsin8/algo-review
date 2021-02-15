e77934ad6385ee46517667c08ff9d1c42f88aa22
rvsin8 <rsingh0815@gmail.com>
2021-02-14 23:14:38 -0500

    remove duplicates from linked lists

--- Desktop/algoexpert/easy/remove_dups_from_linked_list.js
+++ Desktop/algoexpert/easy/remove_dups_from_linked_list.js
@@ -12,3 +12,17 @@
 //O(1)
 
 
+function removeDuplicatesFromLinkedList(linkedList) {
+    let currentNode = linkedList;
+    while (currentNode !== null) {
+        let nextDistinctNode = currentNode.next;
+        while (nextDistinctNode !== null && nextDistinctNode.value === currentNode.value) {
+            nextDistinctNode = nextDistinctNode.next;
+        }
+
+        currentNode.next = nextDistinctNode;
+        currentNode = nextDistinctNode;
+    }
+
+    return linkedList;
+}
