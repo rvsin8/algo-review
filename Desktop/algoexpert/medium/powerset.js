d13b1d02cf94e903c579f466b1830d2f21e5ff03
rvsin8 <rsingh0815@gmail.com>
2021-02-17 06:57:57 -0500

    powerset

--- Desktop/algoexpert/medium/powerset.js
+++ Desktop/algoexpert/medium/powerset.js
@@ -15,26 +15,27 @@
 //we move to 3 so we create [3], [1,3], [2,3], [1,2,3], we added to the empty subset and the subset wt 1 in it, the subset 2 in it, the subset with 1 and 2 in it
 
 //time complexity
-//O(n*2^n)
+//O(n*2^n) how many subsets am i going to have ? there are 2^m subset, we are doubling subsets every time we iterate through and the subset is length n
 
 //space complexity
-//O(n*2^n)
+//O(n*2^n) is the same exact as time bc we are storing 2^n subsets and we have n / 2 elements 
 
+//recursive solution
 function powerset(array, idx = null) {
-    if (idx === null) {
-        idx = array.length - 1;
+    if (idx === null) { //if the idx is none, 
+        idx = array.length - 1; //idx will equal to the length of the array - 1
     }
 
-    if (idx < 0) {
-        return [[]];
+    if (idx < 0) { //if the array is empty we return an empty set
+        return [[]]; //empty set
     }
 
-    const ele = array[idx];
-    const subsets = powerset(array, idx - 1);
-    const length = subsets.length; 
-    for (let i = 0; i < length; i++) {
-        const currentSubset = subsets[i];
-        subsets.push(currentSubset.concat(ele));
+    const ele = array[idx]; //keep track of the ele
+    const subsets = powerset(array, idx - 1); //declare the subsets
+    const length = subsets.length; //keep track of the length of the array
+    for (let i = 0; i < length; i++) { //in range
+        const currentSubset = subsets[i]; //current subset will equal subsets of i
+        subsets.push(currentSubset.concat(ele)); //generate a new subset that is composed of the current subset
     }
     return subsets;
 }
