a559fa5f9df970913316c6d3a1f48cb7772b9b51
rvsin8 <rsingh0815@gmail.com>
2021-02-17 14:47:55 -0500

    lowest common manager

--- Desktop/algoexpert/hard/lowest_common_manager.js
+++ Desktop/algoexpert/hard/lowest_common_manager.js
@@ -5,7 +5,16 @@
 //given three inputs: top one is the manager of some chain with the other two being employees of that company
 //we want the lowest common manager of those 2 employees
 //close to the "youngest common ancestor" problem
+//this question deals with tree-like ds
+//solving this will be clean via recursion
+//for P and I --> B is the lowest common manager
 
+//solution
+//check every subtree and see where the nodes are
+//F E D C = 0 bc they do not have I or P
+//P = 0 + 0 + 1 = 1 under H and B
+//I = 1 under B
+//B = 2
 
 //time complexity 
 //O(n)
