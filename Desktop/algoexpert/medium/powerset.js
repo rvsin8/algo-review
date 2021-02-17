2d127deb93fe445e625a115ccd3b385949477935
rvsin8 <rsingh0815@gmail.com>
2021-02-16 19:46:24 -0500

    powerset

--- Desktop/algoexpert/medium/powerset.js
+++ Desktop/algoexpert/medium/powerset.js
@@ -9,3 +9,20 @@
 
 //space complexity
 //O(n*2^n)
+
+function powerset(array, idx = null) {
+    if (idx === null) {
+        idx = array.length - 1;
+    }
+
+    if (idx < 0) {
+        return [[]];
+    }
+
+    const ele = array[idx];
+    const subsets = powerset(array, idx - 1);
+    const length = subsets.length; 
+    for (let i = 0; i < length; i++) {
+        
+    }
+}
