933a657cb827c325c1719472ce4ad75a8c7ed4b4
rvsin8 <rsingh0815@gmail.com>
2021-02-17 12:58:06 -0500

    reversed linked list

--- Desktop/algoexpert/hard/reverse_linked_list.js
+++ Desktop/algoexpert/hard/reverse_linked_list.js
@@ -7,6 +7,17 @@
 //given a singly linked list, this sll consists of nodes and every node has a ".next" property 
 //the tail points to the none / null value
 //0 --> 1 --> 2 --> 3 --> 4 --> 5 and we want 5 --> 4 --> 3 --> 2 --> 1 --> 0
+//put aside the edge cases like the head and tail, try to understand how you can change or reverse the pointer for one standard node 
+//we can look at 2, its in the middle, has a pointer behind and after it
+//we will call 2 "p" for pointer
+//if we want to reverse the node that means we want the pointer pointing to 3 to point to 1 instead via ".next" property
+//p.next should equal the node that came before it
+//when we are at 2 we have no access to 1
+//we need a variable that will have a reference to the 1
+//we will have p2 at 2 and p1 at 1
+//p2.next = p1 - will lead to 2 --> 1
+//next is the 3 node but we lost our reference to the 3 node from the 2 node bc our p2.next does not equal 3 anymore
+
 
 //time complexity
 //O(n)
