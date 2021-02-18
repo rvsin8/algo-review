ab23c604391d0c18af6ada16cfd706e651c374d3
rvsin8 <rsingh0815@gmail.com>
2021-02-18 11:55:27 -0500

    water area

--- Desktop/algoexpert/hard/water_area.js
+++ Desktop/algoexpert/hard/water_area.js
@@ -11,7 +11,12 @@
 //first idx there is no pillar so no water will be trapped - it will spills over
 //second idx is 8
 //idx3 has pillars to the right of it being 5,10,1,1 and 3
-
+//whether an idx has a zero or positive integer we need to see what is the tallest pillar to the right and left of that idx that is where the water will be trapped
+//what is the min height of the pillars to the right and to the left of the idx
+//so btw pillars wt height 8 and height 10 and the minimum height it 8
+//what is the height of our current idx (idx3) is 0 so water will stored at the height of 8
+//idx5 has a height 5 and 8 - 5 will store height 3 of water
+//have a new array and in that array at each idx store 
 
 //time complexity 
 //O(n) 
