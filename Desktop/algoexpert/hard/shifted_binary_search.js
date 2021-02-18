ec8292a4540dbf4896afad5f3f65810ed20df2d7
rvsin8 <rsingh0815@gmail.com>
2021-02-18 06:57:29 -0500

    shifted binary search

--- Desktop/algoexpert/hard/shifted_binary_search.js
+++ Desktop/algoexpert/hard/shifted_binary_search.js
@@ -11,8 +11,9 @@
 //right pointer at 45 idx 9
 //middle pointer = (0 + 9) / 2 = 4.5 and round it down to idx 4 which is 73
 //usually in bs we compared our middle num 73 to 33, and since 73 is >, we erase the right side but we cannot do that here
-//
-
+//we can look at the leftmost pointer at 45 and realize it is > than 33
+//if the left pointer is < or = our middle pointer then we want to check
+//if our target number is < the middle number AND target num is >= the left num, then we explore the left side
 
 //time complexity
 //O(log(n))
