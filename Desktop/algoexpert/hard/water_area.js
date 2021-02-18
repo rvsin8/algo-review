3a0115be977f74c95e9559fb885572fcce73c098
rvsin8 <rsingh0815@gmail.com>
2021-02-18 11:43:32 -0500

    water area

--- Desktop/algoexpert/hard/water_area.js
+++ Desktop/algoexpert/hard/water_area.js
@@ -2,6 +2,15 @@
 //Dynamic Programming 
 
 //my understanding
+//given a drawing wt an example
+//[0,8,0,5,0,0,10,0,0,1,1,0,3] --> 48
+//each integer represents the height of a pillar
+
+//solution
+//we need to calculate for each idx how much water will be available above that idx
+//first idx there is no pillar so no water will be trapped - it will spills over
+//second idx is 8
+//idx3 has pillars to the right of it being 5,10,1,1 and 3
 
 
 //time complexity 
