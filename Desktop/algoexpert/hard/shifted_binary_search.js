55f344f4aa0c1baac8febbd9efb38ddcfd6e22a4
rvsin8 <rsingh0815@gmail.com>
2021-02-18 06:20:10 -0500

    shifted binary search

--- Desktop/algoexpert/hard/shifted_binary_search.js
+++ Desktop/algoexpert/hard/shifted_binary_search.js
@@ -5,10 +5,12 @@
 
 
 //time complexity
+//O(log(n))
 
 //space complexity
+//O(1)
 
-
+//space being O(log(n))
 function shiftedBinarySearch(array, target) {
     return shiftedBinarySearchHelper(array, target, 0, array.length - 1);
 }
@@ -34,4 +36,37 @@ function shiftedBinarySearchHelper(array, target, left, right) {
             return shiftedBinarySearchHelper(array, target, left, middle - 1);
         }
     }
+}
+
+//more optimal solution 
+//space being O(1)
+
+function shiftedBinarySearch(array, target) {
+    return shiftedBinarySearchHelper(array, target, 0, array.length - 1);
+}
+
+function shiftedBinarySearchHelper(array, target, left, right) {
+    while (left <= right) {
+        const middle = Math.floor((left + right) / 2)
+        const potentialMatch = array[middle];
+        const leftNum = array[left];
+        const rightNum = array[right];
+        if (taeget === potentialMatch) {
+            return middle;
+        } else if (leftNum <= potentialMatch) {
+            if (target < potentialMatch && target >= leftNum) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        } else {
+            if (target > potentialMatch && target <= rightNum) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+    }
+    return -1;
+
 }
