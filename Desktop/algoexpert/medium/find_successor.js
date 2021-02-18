0079856f2df9a9acf0226cb05b033cbd8da494ae
rvsin8 <rsingh0815@gmail.com>
2021-02-18 08:03:51 -0500

    shifted binary search resubmit

--- Desktop/algoexpert/medium/find_successor.js
+++ Desktop/algoexpert/medium/find_successor.js
@@ -14,3 +14,19 @@ function findSuccessor(tree, node) {
     return getRightmostParent(node);
 }
 
+function getLeftmostChild(node) {
+    let currentNode = node;
+    while (currentNode.left !== null) {
+        currentNode = currentNode.left;
+    }
+    return currentNode;
+}
+
+function getRightmostParent(node) {
+    let currentNode = node;
+    while (currentNode.parent !== null && currentNode.parent.right === currentNode) {
+        currentNode = currentNode.parent;
+    }
+
+    return currentNode.parent;
+}
