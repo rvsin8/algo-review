cf4b5920a3e0f00d301367607de20bf0def00d69
rvsin8 <rsingh0815@gmail.com>
2021-02-18 06:05:50 -0500

    shifted binary search

--- Desktop/algoexpert/hard/shifted_binary_search.js
+++ Desktop/algoexpert/hard/shifted_binary_search.js
@@ -12,3 +12,14 @@
 function shiftedBinarySearch(array, target) {
     return shiftedBinarySearchHelper(array, target, 0, array.length - 1);
 }
+
+function shiftedBinarySearchHelper(array, target, left, right) {
+    if (left > right) return -1;
+    const middle = Math.floor((left + right) / 2)
+    const potentialMatch = array[middle];
+    const leftNum = array[left];
+    const rightNum = array[right];
+    if (taeget === potentialMatch) {
+        return middle;
+    }
+}
