006c445ee9cc680e91b04be357314816b4e0cad5
rvsin8 <rsingh0815@gmail.com>
2021-02-19 10:41:33 -0500

    numbers in pi

--- Desktop/algoexpert/hard/numbers_in_pi.js
+++ Desktop/algoexpert/hard/numbers_in_pi.js
@@ -9,3 +9,11 @@
 //space complexity
 //O(n + m)
 
+function numbersInPi(pi, numbers) {
+    const numbersTable = {};
+    for (const number of numbers) {
+        numbersTable[number] = true;
+    }
+    const minSpaces = getMinspaces(pi, numbersTable, {}, 0);
+    return minSpaces ===Infinity ? -1 : minSpaces;
+}
