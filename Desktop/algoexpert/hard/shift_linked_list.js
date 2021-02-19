620bbef778a2ef126200a0d3993c74eb59ff96a9
rvsin8 <rsingh0815@gmail.com>
2021-02-19 10:33:16 -0500

    shift linked list

--- Desktop/algoexpert/hard/shift_linked_list.js
+++ Desktop/algoexpert/hard/shift_linked_list.js
@@ -15,27 +15,31 @@
 //what exactly what we just did entails ? we made 3 into a tail node and 5 a regular node with a pointer
 //we care about four nodes - tail node, middle node that will be the new tail node, original head of the node and the new head node
 //calculate the length of the linked list via a counter and once we have it - 6 - we do 6 - k which is 2 = 4, our new tail will be in pos 4
-//
+//position --> pnt = length - k 
+//we are  not storing extra data
+//when k is 0, we return the linked list
+//when k is very large like 30 - we can use a module operator - 30 % 6 = 0 we do nothing 
+//when k is a negative number like -2 - we move backwards - pnt = 0 + k is the new formula and we will alsop use the module operator 
 
 //time complexity 
-//O(n)
+//O(n) n is the length of the linked list
 
 //space complexity 
-//O(1)
+//O(1) no extra storage, everything is in place
 
 function shifLinkedList(head, k) {
-    let listLength = 1;
-    let listTail = head;
-    while (listTail.next !== null) {
-        listTail = listTail.next;
-        listLength++;
+    let listLength = 1; //our counter
+    let listTail = head; //our current node
+    while (listTail.next !== null) { //aslong as the list tail is not none
+        listTail = listTail.next; //our list tail is next
+        listLength++; //keep incrementing 
     }
 
-    const offset = Math.abs(k) % listLength;
-    if (offset === 0) return head;
+    const offset = Math.abs(k) % listLength; //calculate the offset where it is the abs value of k module the list length
+    if (offset === 0) return head; //if offset is 0 we return the head
 
-    const newTailPosition = k > 0 ? listLength - offset : offset;
-    let newTail = head;
+    const newTailPosition = k > 0 ? listLength - offset : offset; //positive or negative integer
+    let newTail = head; //update 
     for (let i = 1; i < newTailPosition; i++) {
         newTail = newTail.next;
     }
@@ -45,3 +49,5 @@ function shifLinkedList(head, k) {
     listTail.next = head;
     return newHead;
 }
+
+//review this cause wtf
