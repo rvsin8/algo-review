192a257bb63f17f8cdfdb7042ce67702872d2d64
rvsin8 <rsingh0815@gmail.com>
2021-02-22 16:24:56 -0500

    height balanced binary tree

--- Desktop/algoexpert/medium/height_balanced_bt.js
+++ Desktop/algoexpert/medium/height_balanced_bt.js
@@ -2,6 +2,16 @@
 //Binary Trees
 
 //my understanding 
+//given the root node and we are suppose to analyze the tree and return true or false if the tree is balanced or not
+//a bts is considered to be height balanced if for every single node in the tree the difference of the height of that node's left subtree is equal to 1 and not greater
+
+//my solution
+//we can't make assumptions solely based off of root nodes
+//we want to go level by level and check each node's subtree by looking to the left and right
+//determine the height of the left subtree and right subtree
+//we use recursion 
+//when we check each subtree we return to the parent node and depending on how many traversals were done we know the height
+
 
 //time complexity
 //O(n)
@@ -18,7 +28,7 @@ function getTreeInfo(node) {
     if (node === null) return new TreeInfo(true, -1);
 
     const leftSubTreeInfo = getTreeInfo(node.left);
-    const rightSubtree = getTreeInfo(node.right);
+    const rightSubtreeInfo = getTreeInfo(node.right);
 
     const isBalanced = 
         leftSubtreeInfo.isBalanced &&
