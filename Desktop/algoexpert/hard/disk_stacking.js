5be7170e265f53ce544f3f799a7c1e926006d159
rvsin8 <rsingh0815@gmail.com>
2021-02-22 12:40:30 -0500

    disk stacking

--- Desktop/algoexpert/hard/disk_stacking.js
+++ Desktop/algoexpert/hard/disk_stacking.js
@@ -16,6 +16,17 @@ function diskStacking(disks) {
     const sequences = new Array(this.length);
     let maxHeightIdx = 0;
     for (let i = 1; i < disk.length; i++) {
-        
+        const currentDisk = disks[i];
+        for (let j = 0; j < i; j++) {
+            const otherDisk = disks[j];
+            if (AreValidDimensions(otherDisk, currentDisk)) {
+                if (heights[i] <= currentDisk[2] + height[j]) {
+                    heights[i] = currentDisk[2] + height[j];
+                    sequences[i] = j;
+                }
+            }
+        }
+        if (heights[i] >= heights[maxHeightIdx]) maxHeightIdx = i;
     }
+    return buildSequence(disks, sequences, maxHeightIdx);
 }
