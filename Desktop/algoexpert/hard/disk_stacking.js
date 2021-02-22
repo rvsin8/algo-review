992e784c515ef42a1381b4082bf6943a84353269
rvsin8 <rsingh0815@gmail.com>
2021-02-22 13:14:26 -0500

    disk stacking

--- Desktop/algoexpert/hard/disk_stacking.js
+++ Desktop/algoexpert/hard/disk_stacking.js
@@ -2,6 +2,22 @@
 //Dynamic Programming
 
 //my understanding
+//our input contains arrays in which there are subarray containing three numbers
+//these integers represent dimensions of the disks, the first integer is the width, second is the depth and the last is the height
+//we have to stack up the disks and create the tallest stack we can - if we want to want to put one disk on top of another its width will have to be smaller along with the disk and height
+//no turning or rotating disks
+
+//my solution
+//[[2,2,1], [2,1,2], [3,2,2], [2,3,4], [4,4,5], [2,2,8]]
+//to make it easier to solve it, the first step is to sort the arrays in ascending order by height
+//[1,2,3,4,5,8] heights array
+//we are progressively going to update this array to see if we get higher heights
+//if we look at idx0 [2,2,1] we cannot stack anything on top of it bc it has the lowest height so it stays 1
+//at idx1 [2,1,2], the width of it is 2 and the width of the disk below is 2 so we are at 2
+//idx2 [3,2,3] and compare it is the array in idx1 3 > 2 width, 2 > 1 and 3 > 2 yes this can be stacked with the array at idx1 but it cannot be stacked on the array of idx0 //update the height to 5 bc the height is 2 + 3
+//currentDisk = array[i] for o < i < length
+//otherDisk = array[j] for o < j < i
+//if w0 < wc and d0 < dc and h0 < hc : height[i] = max heights[i] or hc +heights[j]
 
 
 //time complexity
