3b06e9e25dba6b550daed82a3b22208a9c5d56e8
rvsin8 <rsingh0815@gmail.com>
2021-02-22 12:36:17 -0500

    disk stacking

--- Desktop/algoexpert/hard/disk_stacking.js
+++ Desktop/algoexpert/hard/disk_stacking.js
@@ -9,3 +9,13 @@
 
 //space complexity 
 //O(n)
+
+function diskStacking(disks) {
+    disks.sort((a,b) => a[2] - b[2]);
+    const heights = disks.map(disk => disk[2]);
+    const sequences = new Array(this.length);
+    let maxHeightIdx = 0;
+    for (let i = 1; i < disk.length; i++) {
+        
+    }
+}
