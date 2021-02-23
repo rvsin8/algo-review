3d14bd589bb27e55433f0a40898428987b0b8a78
rvsin8 <rsingh0815@gmail.com>
2021-02-23 16:30:37 -0500

    solve sudoku

--- Desktop/algoexpert/hard/solve_sudoku.js
+++ Desktop/algoexpert/hard/solve_sudoku.js
@@ -6,14 +6,20 @@
 //0 will be an empty digit
 //a sudoku puzzle contains 9 subgrids that can only contain 1-9 integers 
 //we cannot have the same digits in the same grid, col or row
+//can modify this in place
+
+//my solution
+//each empty place has 9 opportunity in digits
+//cannot use brute force bc we would have to create every board imagined and see what board follows the rules and this cannot be done efficient at all
+//use back-tracking when a square has trouble filling up and back track is the way we will find the solution
 //
 
 
 //time complexity 
-//O(1)
+//O(1) we know the size of our input array is always going to be 9x9 a constant size board
 
 //space complexity 
-//O(1)
+//O(1) we are going to modify the input array
 
 
 function solveSudoku(board) {
