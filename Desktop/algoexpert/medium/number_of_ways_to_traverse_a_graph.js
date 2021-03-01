8f9a0100eee0c7724d7b5a33c6c0b833466c5932
rvsin8 <rsingh0815@gmail.com>
2021-03-01 13:43:35 -0500

    number of ways to traverse a graph

--- Desktop/algoexpert/medium/number_of_ways_to_traverse_a_graph.js
+++ Desktop/algoexpert/medium/number_of_ways_to_traverse_a_graph.js
@@ -2,6 +2,13 @@
 //Dynamic Programming 
 
 //my understanding 
+//we are given two inputs a height and a width and these will be positive non-zero integers 
+//top left is starting pos and bottom right is our ending position and we must see how many traverses it takes to go from the start to bottom
+//can only go right and down 
+//cannot go up or left 
+//return the num of ways when you can go to the end
+//width will have to be always atleast 2
+//width of 4 and height of 3 can be done 1o ways 
 
 //time complexity 
 //O(n + m)
