c55de41698560929f73a3dc327f4b6ff5daa381a
rvsin8 <rsingh0815@gmail.com>
2021-03-01 13:46:54 -0500

    number of ways to traverse a graph

--- Desktop/algoexpert/medium/number_of_ways_to_traverse_a_graph.js
+++ Desktop/algoexpert/medium/number_of_ways_to_traverse_a_graph.js
@@ -7,8 +7,15 @@
 //can only go right and down 
 //cannot go up or left 
 //return the num of ways when you can go to the end
-//width will have to be always atleast 2
+//width will have to be always at least 2
 //width of 4 and height of 3 can be done 1o ways 
+
+//my solution 
+//many diff ways to solve this
+//you can do it recursive, trick and dynamic programming 
+//recursive way --> if we can find the num of ways to get to the position to the left or above the end point we essentially have figured this out
+//sum the top and left of each position to see how to get to each square left or above the end
+//
 
 //time complexity 
 //O(n + m)
@@ -16,6 +23,7 @@
 //space complexity 
 //O(1)
 
+//trick solution 
 function numberOfWaysToTraverseGraph(width, height) {
     const xDistanceToCorner = width - 1;
     const yDistanceToCorner = height - 1;
