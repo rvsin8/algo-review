2603dc24c574c87e07340e34fe55cdc8ca5659a5
rvsin8 <rsingh0815@gmail.com>
2021-03-01 13:35:12 -0500

    number of ways to traverse a graph

--- Desktop/algoexpert/medium/number_of_ways_to_traverse_a_graph.js
+++ Desktop/algoexpert/medium/number_of_ways_to_traverse_a_graph.js
@@ -4,6 +4,17 @@
 //my understanding 
 
 //time complexity 
+//O(n + m)
 
 //space complexity 
+//O(1)
 
+function numberOfWaysToTraverseGraph(width, height) {
+    const xDistanceToCorner = width - 1;
+    const yDistanceToCorner = height - 1;
+
+    const numerator = factorial(xDistanceToCorner + yDistanceToCorner);
+    const denominator = factorial(xDistanceToCorner) * factorial(yDistanceToCorner);
+    return Math.floor(numerator / denominator);
+    
+}
