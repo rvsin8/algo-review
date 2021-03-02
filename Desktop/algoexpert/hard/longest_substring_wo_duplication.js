0250f48944d41c0665c3b0730f3883f169ba82ab
rvsin8 <rsingh0815@gmail.com>
2021-03-02 16:21:53 -0500

    longest substring without duplication

--- Desktop/algoexpert/hard/longest_substring_wo_duplication.js
+++ Desktop/algoexpert/hard/longest_substring_wo_duplication.js
@@ -12,6 +12,9 @@
 //once we find a dup we update the start idx //startidx = max(startidx, lastseen[currentletter] + 1) //last seen is "clem"
 //since e was last seen, so e was at 2 we add it by 1 and see if its greater and if it is we start from there 
 //startIdx = max(0, 2+1) --> 3 so we move to idx3 aka m and start from there
+//right now "me" is not greater than clem so we do not update but we move on to the next letter after me which is n
+//update hash table always so now e: 4, n:5 and t:6 and i:7 are added to the hash table and menti is > clem so we not update the longest substr to "menti" and keep going to "mentisac"
+//
 
 //time complexity 
 //O(n)
