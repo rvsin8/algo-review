ea10e77547a58633d5b2f11276de6f8dbcdc9f85
rvsin8 <rsingh0815@gmail.com>
2021-03-02 16:09:51 -0500

    longest substring without duplication

--- Desktop/algoexpert/hard/longest_substring_wo_duplication.js
+++ Desktop/algoexpert/hard/longest_substring_wo_duplication.js
@@ -15,5 +15,13 @@ function longestSubstringWithoutDuplication(string) {
     let startIdx = 0;
     for (let i = 0; i < string.length; i++) {
         const char = string[i];
+        if (char in lastSeen) {
+            startIdx = Math.max(startIdx, lastSeen[char] + 1);
+        }
+        if (longest[1] - longest[0] < i + 1 - startIdx) {
+            longest = [startIdx, i + 1];
+        }
+        lastSeen[char] = i;
     }
+    return string.slice(longest[0], longest[1]);
 }
