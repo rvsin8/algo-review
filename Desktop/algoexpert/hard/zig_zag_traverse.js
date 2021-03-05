b32ebacb5aa15256e462df88f0c7209799656978
rvsin8 <rsingh0815@gmail.com>
2021-03-04 19:06:14 -0500

    zig zag traverse

--- Desktop/algoexpert/hard/zig_zag_traverse.js
+++ Desktop/algoexpert/hard/zig_zag_traverse.js
@@ -9,3 +9,35 @@
 //space complexity
 //O(n)
 
+function zigzagTraverse(array) {
+    const height = array.length - 1;
+    const width = array[0].length - 1;
+    const result = [];
+    let row = 0;
+    let col = 0;
+    let goingDown = true;
+    while (!isOutOfBounds(row, col, height, width)) {
+        result.push(array[row][col]);
+        if (goingDown) {
+            if (col === 0 || row === height) {
+                goingDown = false;
+                if (row === height) {
+                    col++;
+                } else {
+                    row++;
+                }
+            } else {
+                row++;
+                col--;
+            }
+        } else {
+            if (row === 0 || col === width) {
+                row++;
+            } else {
+                row--;
+                col++;
+            }
+        }
+    }
+    return result;
+}
