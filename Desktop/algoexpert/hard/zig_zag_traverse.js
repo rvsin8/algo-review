b106d31cfc0fb87251d074191d3fe99687ac5811
rvsin8 <rsingh0815@gmail.com>
2021-03-05 11:02:47 -0500

    zig zag traverse

--- Desktop/algoexpert/hard/zig_zag_traverse.js
+++ Desktop/algoexpert/hard/zig_zag_traverse.js
@@ -5,6 +5,10 @@
 //given a 2D array and we need to traverse it in a zig zag order
 //this question is easier in face value than actual implementation 
 //there are only two directions you are going in which is down or up maybe even right
+//1-2 4-6 10-13 15-16 you are going down
+//2-3 7-10 14-15 you are going up
+//3-4 13-14 you are going to the right
+//we always go down at the parameters 
 
 //time complexity 
 //O(n)
