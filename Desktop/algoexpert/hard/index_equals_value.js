bbeb85cae3fa9ba2439989c84004d4188a176890
rvsin8 <rsingh0815@gmail.com>
2021-03-08 15:16:13 -0500

    index equals value

--- Desktop/algoexpert/hard/index_equals_value.js
+++ Desktop/algoexpert/hard/index_equals_value.js
@@ -9,3 +9,12 @@
 //space complexity 
 //O(1)
 
+function indexEqualsValue(array) {
+    let leftIndex = 0;
+    let rightIndex = array.length - 1;
+
+    while (leftIndex <= rightIndex) {
+        const middleIndex = leftIndex + Math.floor((rightIndex - leftIndex) / 2);
+        const middleValue = array[middleIndex];
+    }
+}
