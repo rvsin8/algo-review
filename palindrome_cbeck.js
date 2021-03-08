79186936df90be00b8a299fc755b8ac3f4b33a08
rvsin8 <rsingh0815@gmail.com>
2021-03-08 10:33:06 -0500

    palindrome check

--- palindrome_cbeck.js
+++ palindrome_cbeck.js
@@ -4,7 +4,9 @@
 //my understanding
 //a palindrome is a string written the same forward as backwards
 //can solve it in a lot of ways
-//straightforward way - create a new string going backwards and compare it to the original string 
+//straightforward way - create a new string going backwards and compare it to the original string //O(n^2) bc when we build up a new string via traversal in the original string and then we compare after 
+//second way, instead of creating a new string - make a new list and then do a comparison - when we do our iteration  backwards and append the letters to the array //o(n) time
+//third way is recursion, if the first letter is = to the last letter and our ispalindrome function is valid for the middle value then we return true //O(N)
 //
 
 //time complexity 
