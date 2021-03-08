4860ece8fe515632ffe85acc526151044a71ac6e
rvsin8 <rsingh0815@gmail.com>
2021-03-08 17:27:27 -0500

    run length encoding

--- Desktop/algoexpert/easy/run_length_encoding.js
+++ Desktop/algoexpert/easy/run_length_encoding.js
@@ -5,6 +5,9 @@
 //lost list - when you can use to output to get back to the input and vice versa ?
 //"13A" is confusing bc how do we know thr 1 and 3 are separate so we say 9A4A this is why its important to split it up
 //our goal to to take this input AAAAAAAAAAAAABBCCCCDD --> and get this output 9A4A2B4C2D
+//chars = [] we dont use a string bc it is immutable which means anything to change it will require an O(N)
+//
+//
 
 //time complexity 
 //O(n)
