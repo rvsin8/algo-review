11441da823aa1e85a71b5cdb19334d753b9077ae
rvsin8 <rsingh0815@gmail.com>
2021-03-08 17:06:02 -0500

    run length encoding

--- Desktop/algoexpert/easy/run_length_encoding.js
+++ Desktop/algoexpert/easy/run_length_encoding.js
@@ -14,6 +14,18 @@ function runLengthEncoding(string) {
     let currentRunLength = 1;
 
     for (let i = 1; i < string.length; i++) {
-        
+        const currentCharacter = string[i];
+        const previousCharacter = string[i - 1];
+
+        if (currentCharacter !== previousCharacter || currentRunLength === 9) {
+            encodedStringCharacters.push(currentRunLength.toString());
+            encodedStringCharacters.push(previousCharacter);
+            currentRunLength = 0;
+        }
+
+        currentRunLength++;
+
     }
+
+    
 }
