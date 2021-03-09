1c6fb9da1b895fb7b31089777f24859a01ffcd49
rvsin8 <rsingh0815@gmail.com>
2021-03-09 16:32:11 -0500

    find kth largest value in bst

--- Desktop/algoexpert/medium/find_kth_largest_value_in_bst.js
+++ Desktop/algoexpert/medium/find_kth_largest_value_in_bst.js
@@ -6,7 +6,18 @@
 //k=3 --> 17 bc its the third largest value
 //all our nodes in our binary search tree will be represented by a class and our nodes will have a left and right node and a value
 //K will always be <= to the total nodes
-//
+//brute force will lead to O(n) time and space
+//we can look at the inorder traversal and find the k highest
+//in order-traversal --> we go left, visit the node and then right
+//15 is root so we go left to 5 then left to 2 and then left to 1 and there is no more node so we add 1 to the array --> [1]
+//then we go to node 2 we add and then we go to the right to 3 and add --> [1,2,3]
+//back up to 5 --> [1,2,3,4,5] and we go right and add the second 5 --> [1,2,3,4,5,5]
+//back up to `15 our root node --> [1,2,3,4,5,5,15] and we go right and so on until we have [1,2,3,4,5,5,15,17,20,22]
+//an inorder traversal on a binary search tree will give you an ordered list so its easy to fint he kth highest node
+//this solution is O(N) space and time bc we need to traverse through every node in our bst
+
+//my solution 
+
 
 //time complexity 
 //O(h + k)
