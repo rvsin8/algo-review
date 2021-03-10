d43a7a03eabc141691e5e4a480996794f269ace3
rvsin8 <rsingh0815@gmail.com>
2021-03-10 13:24:55 -0500

    sum of linked lists

--- Desktop/algoexpert/medium/sum_of_linked_lists.js
+++ Desktop/algoexpert/medium/sum_of_linked_lists.js
@@ -10,5 +10,10 @@
 //O(max(n,m))
 
 function sumOfLinkedLists(linkedListOne, linkedListTwo) {
-    
+    const newLinkedListHeadPointer = new linkedListOne(0);
+    let currentNode = newLinkedListHeadPointer;
+    let carry = 0;
+
+    let nodeOne = linkedListOne;
+    let nodeTwo = linkedListTwo;
 }
