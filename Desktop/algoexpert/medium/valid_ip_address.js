cfd9cd649c5a00bf8cfe459b2fefbb4be4b22253
rvsin8 <rsingh0815@gmail.com>
2021-03-10 18:07:33 -0500

    valid ip address

--- Desktop/algoexpert/medium/valid_ip_address.js
+++ Desktop/algoexpert/medium/valid_ip_address.js
@@ -18,6 +18,15 @@ function validIPAddresses(string) {
         currentIPAddressParts[0] = string.slice(0,i);
         if (!isValidPart(currentIPAddressParts[0])) continue;
 
-        
+        for (let j = i + 1; j < i + Math.min(string.length - i, 4); j++) {
+            currentIPAddressParts[1] = string.slice(i,j);
+            if (!isValidPart(currentIPAddressParts[1])) continue;
+
+            for (let k = j + 1; k < j + Math.min(string.length - j, 4); k++) {
+                
+            }
+        }
+
+
     }
 }
