d14c52e03e9641e40aa2dccae3dd9c91cebeac40
rvsin8 <rsingh0815@gmail.com>
2021-03-15 19:43:05 -0400

    two sums

--- Desktop/algoexpert/easy/two_sum.js
+++ Desktop/algoexpert/easy/two_sum.js
@@ -6,7 +6,11 @@
 //a separate integers value that gives you a target sum
 //we need to see if there is a pair in the array that sums up to the target sum
 //[3,5,-4,8,11,1,-1,6], 10 --> 11 - 1
-
+//we can use two for loops but its not optimal for a time point of view
+//a better way may be a hash table, may cost extra space but will make our time complexity better 
+//we can create a hash table and traverse our array and at each number we stop at see if it equals our target num
+//we can have targetSum = 10, and have currentNum = x and find y, where x + y = 10
+//
 
 //time complexity 
 //O(nlog(n))
