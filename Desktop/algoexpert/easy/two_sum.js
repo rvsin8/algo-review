feadd8e8edb05f58966c4c01b3949ae603dc4fef
rvsin8 <rsingh0815@gmail.com>
2021-03-15 19:22:50 -0400

    two sums

--- Desktop/algoexpert/easy/two_sum.js
+++ Desktop/algoexpert/easy/two_sum.js
@@ -14,8 +14,13 @@ function twoNumberSum(array, targetSum) {
     let left = 0;
     let right = array.length - 1;
     while (left < right) {
-        
+        const currentSum = array[left] + array[right];
+        if (currentSum === targetSum) {
+            return [array[left], array[right]];
+    } else if (currentSum < targetSum) {
+        left++;
+    } else if (currentSum > targetSum) {
+        right--;
     }
-
 }
 
