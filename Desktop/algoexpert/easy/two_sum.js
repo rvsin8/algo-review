1be5e07255b343986b49c2b3c134a85390288087
rvsin8 <rsingh0815@gmail.com>
2021-03-15 19:19:46 -0400

    two sums

--- Desktop/algoexpert/easy/two_sum.js
+++ Desktop/algoexpert/easy/two_sum.js
@@ -9,5 +9,13 @@
 //space complexity 
 //O(1)
 
+function twoNumberSum(array, targetSum) {
+    array.sort((a,b) => a - b);
+    let left = 0;
+    let right = array.length - 1;
+    while (left < right) {
+        
+    }
 
+}
 
