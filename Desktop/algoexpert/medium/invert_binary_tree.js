f5cd04502a705d94991739c606449e089bbf82cd
rvsin8 <rsingh0815@gmail.com>
2021-03-16 14:43:06 -0400

    invert binary tree

--- Desktop/algoexpert/medium/invert_binary_tree.js
+++ Desktop/algoexpert/medium/invert_binary_tree.js
@@ -6,7 +6,11 @@
 //gives us a binary tree, every node has a value integer value and every node has 0 1 or 2 children nodes
 //invert this tree
 //swap the left and right nodes
-
+//iterative solution deals with like a breadth search first algo
+//we can also do it recursively 
+//we want to initialize a queue bc we want to traverse it level by level from root to the children - level by level 
+//traverse in breadth first search manner
+//
 
 //time complexity
 //O(n)
