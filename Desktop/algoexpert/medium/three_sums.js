127f03b669987f062984a83d736e11e75ef365b0
rvsin8 <rsingh0815@gmail.com>
2021-03-16 18:21:09 -0400

    three sums

--- Desktop/algoexpert/medium/three_sums.js
+++ Desktop/algoexpert/medium/three_sums.js
@@ -15,6 +15,19 @@ function threeNumberSum(array, targetSum) {
     for (let i = 0; i < array.length - 2; i++) {
         let left = i + 1;
         let right = array.length - 1;
+        while (left < right) {
+            const currentSum = array[i] + array[left] + array[right];
+            if (currentSum === targetSum) {
+                triplets.push([array[i], array[left], array[right]]);
+                left++;
+                right--;
+            } else if (currentSum < taegetSum) {
+                left++;
+            } else if (currentSum > targetSum) {
+                right--;
+            }
+        }
     }
+    return triplets;
 
 }
