3ac9639f412189ed588400e5090315268ea96626
rvsin8 <rsingh0815@gmail.com>
2021-03-16 15:34:48 -0400

    invert binary tree

--- Desktop/algoexpert/medium/invert_binary_tree.js
+++ Desktop/algoexpert/medium/invert_binary_tree.js
@@ -35,3 +35,20 @@ function swapLeftAndRight(tree) {
     tree.right = left;
 }
 
+//iterative solution 
+function invertBinaryTree(tree) {
+    const queue = [tree];
+    while (queue.length) {
+        const current = queue.shift();
+        if (current === null) continue;
+        swapLeftAndRight(current);
+        queue.push(current.left);
+        queue.push(current.right);
+    }
+}
+
+function swapLeftAndRight(tree) {
+    const left = tree.left;
+    tree.left = tree.right;
+    tree.right = left;
+}
