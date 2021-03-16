648671a6f5c1480ea717a46578a395d6ce0588a9
rvsin8 <rsingh0815@gmail.com>
2021-03-16 18:20:18 -0400

    three sums

--- Desktop/algoexpert/medium/three_sums.js
+++ Desktop/algoexpert/medium/three_sums.js
@@ -10,5 +10,11 @@
 //O(n)
 
 function threeNumberSum(array, targetSum) {
-    
+    array.sort((a,b) => a - b);
+    const triplets = [];
+    for (let i = 0; i < array.length - 2; i++) {
+        let left = i + 1;
+        let right = array.length - 1;
+    }
+
 }
