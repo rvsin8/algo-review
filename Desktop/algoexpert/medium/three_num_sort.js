fa7e772a090625d1e7f734daa9bf929794f41093
rvsin8 <rsingh0815@gmail.com>
2021-03-17 12:22:52 -0400

    three num sort

--- Desktop/algoexpert/medium/three_num_sort.js
+++ Desktop/algoexpert/medium/three_num_sort.js
@@ -9,3 +9,11 @@
 //space complexity 
 //O(1)
 
+function threeNumberSort(array, order) {
+    const valueCounts = [0,0,0];
+
+    for (const element of array) {
+        const orderIdx = prder.indexOf(element);
+        valueCounts[orderIdx]++;
+    }
+}
