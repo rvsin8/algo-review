1f56cf3ecbad863655b7bdf313bef3354bfdd656
rvsin8 <rsingh0815@gmail.com>
2021-03-17 16:11:51 -0400

    heap sort

--- Desktop/algoexpert/hard/heap_sort.js
+++ Desktop/algoexpert/hard/heap_sort.js
@@ -9,3 +9,11 @@
 //space complexity 
 //O(nlog(n))
 
+function heapSort(array) {
+    buildMaxHeap(array);
+    for (let endIdx = array.length - 1; endIdx > 0; endIdx--) {
+        swap(0, endIdx, array);
+        siftDown(0, endIdx - 1, array);
+    }
+    return array;
+}
