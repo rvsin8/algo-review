c96eb38b3e35fbc1aa6a73ea3e5707a0991650b8
rvsin8 <rsingh0815@gmail.com>
2021-03-16 23:14:11 -0400

    three sums

--- Desktop/algoexpert/medium/three_sums.js
+++ Desktop/algoexpert/medium/three_sums.js
@@ -4,7 +4,12 @@
 //my understandings 
 //find all the possible triplets that sum up to the target sum
 //[12,3,1,2,-6,5,-8,6], 0 --> [[-8,2,6], [-8,3,5], [-6,1,5]]
-//
+//we can do this by sorting this in ascending order and using two pointers - left / right
+//[-8,-6,1,2,3,5,6,12], 0
+//iterate through the array once 
+//we start with -8
+//have a left pointer one ele ahead of -8 at -6
+//have a right pointer at the end of the array at 12
 
 //time complexity 
 //O(n^2)
