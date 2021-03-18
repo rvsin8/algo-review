9208ef3cff024eac4a7e37dcdd7cf615ffee2380
rvsin8 <rsingh0815@gmail.com>
2021-03-18 17:36:57 -0400

    sorted squared array

--- Desktop/algoexpert/easy/sorted_squared_array.js
+++ Desktop/algoexpert/easy/sorted_squared_array.js
@@ -9,5 +9,12 @@
 //space complexity 
 //O(n)
 
+function sortedSquaredArray(array) {
+    const sortedSquares = new Array(array.length).fill(0);
 
+    for (let idx = 0; idx < array.length; idx++) {
+        const value = array[idx];
+        sortedSquares[idx] = value * value;
+    }
+}
 
