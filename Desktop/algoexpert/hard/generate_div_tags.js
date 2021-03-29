676251468a504466d90cc5bc630c0d417d33a96f
rvsin8 <rsingh0815@gmail.com>
2021-03-29 11:49:15 -0400

    generate div tags

--- Desktop/algoexpert/hard/generate_div_tags.js
+++ Desktop/algoexpert/hard/generate_div_tags.js
@@ -17,5 +17,16 @@ function generateDivTags(numberOfTags) {
 }
 
 function generateDivTagsFromPrefix(openingTagsNeeded, closingTagsNeeded, prefix, result) {
-    
+    if (openingTagesNeeded > 0) {
+        const newPrefix = prefix + '<div>';
+        generateDivTagsFromPrefix(openingTagsNeeded - 1, closingTagsNeeded - 1, newPrefix, result);
+    }
+
+    if (openingTagsNeeded < closingTagsNeeded) {
+        const newPrefix = prefix + '<div>';
+        generateDivTagsFromPrefix(openingTagsNeeded, closingTagsNeeded - 1, newPrefix, result);
+    }
+
+    if (closingTagsNeeded === 0) result.push(prefix);
+
 }
