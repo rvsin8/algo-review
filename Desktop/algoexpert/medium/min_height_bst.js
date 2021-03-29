103c4ba63e2c464fa45d3f15ecced5fc4f519205
rvsin8 <rsingh0815@gmail.com>
2021-03-29 14:03:46 -0400

    minimum height bst

--- Desktop/algoexpert/medium/min_height_bst.js
+++ Desktop/algoexpert/medium/min_height_bst.js
@@ -13,4 +13,11 @@ function minHeightBst(array) {
     return constructMinHeightBst(array, 0, array.length - 1);
 }
 
-
+function constructMinHeightBst(array, startIdx, endIdx) {
+    if (endIdx < startIdx) return null;
+    const midIdx = Math.floor((startIdx + endIdx) / 2);
+    const bst = new BST(array[midIdx]);
+    bst.left = constructMinHeightBst(array, startIdx, midIdx - 1);
+    bst.right = constructMinHeightBst(array, midIdx + 1, endIdx);
+    return bst;
+}
