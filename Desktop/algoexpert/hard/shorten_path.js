2d1e32c79e357362d67235df56cba2afc2f3fd53
rvsin8 <rsingh0815@gmail.com>
2021-04-01 13:51:21 -0400

    interweaving strings

--- Desktop/algoexpert/hard/shorten_path.js
+++ Desktop/algoexpert/hard/shorten_path.js
@@ -9,3 +9,8 @@
 //space complexity 
 //O(n)
 
+function shortenPath(path) {
+    const startWithSlash = path[0] === '/';
+    const token = paths.split('/').filter(isImportantToken);
+    const stack = [];
+}
