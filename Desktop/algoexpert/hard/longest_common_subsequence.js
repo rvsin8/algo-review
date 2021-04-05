95e0bfc426001a2677fa90a135314ca0b1d0f98a
rvsin8 <rsingh0815@gmail.com>
2021-04-05 15:53:32 -0400

    longest common subsequence

--- Desktop/algoexpert/hard/longest_common_subsequence.js
+++ Desktop/algoexpert/hard/longest_common_subsequence.js
@@ -12,5 +12,13 @@ function longestCommonSubsequence(str1, str2) {
     for (let i = 0; i < str2.length + 1; i++) {
         lengths.push(new Array(str1.length + 1).fill(0));
     }
-    
+    for (let i = 1; i < str2.length + 1; i++) {
+        for (let j = 1; j < str1.length + 1; j++) {
+            if (str2[i - 1] === str1[j - 1]) {
+                lengths[i][j] = lengths[i - 1][j - 1] + 1;
+            } else {
+                lengths[i][j] = Math.max(lengths[i - 1][j], lengths[i][j - 1]);
+            }
+        }
+    }
 }
