4727f99dfd80f4195538ee010eda3859d67207fa
rvsin8 <rsingh0815@gmail.com>
2021-04-12 17:25:18 -0400

    underscorify substring

--- Desktop/algoexpert/hard/underscorify_substring.js
+++ Desktop/algoexpert/hard/underscorify_substring.js
@@ -13,3 +13,14 @@ function underscorfiySubstring(string, substring) {
     const locations = collapse(getLocations(strings, substring));
     return underscorify(string, locations);
 }
+
+function getLocations(string, substring) {
+    const locations = [];
+    let startIdx = 0;
+    while (startIdx < string.length) {
+        const nextIdx = string.indexof(substring, startIdx);
+        if (nextIdx !== -1) {
+            
+        }
+    }
+}
