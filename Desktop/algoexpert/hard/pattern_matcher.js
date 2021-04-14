4f11dd13cd9a63e39153032d1df255f6a5d0699d
rvsin8 <rsingh0815@gmail.com>
2021-04-14 12:39:27 -0400

    pattern matcher

--- Desktop/algoexpert/hard/pattern_matcher.js
+++ Desktop/algoexpert/hard/pattern_matcher.js
@@ -15,4 +15,18 @@ function patternMatch(pattern, string) {
     const didSwitch = newPattern[0] !== pattern[0];
     const counts = {x: 0, y: 0};
     const firstYPos = getCountsAndFirstYPos(newPattern, counts);
+    if (counts['y'] !== 0) {
+        for (let lenOfX = 1; lenOfX < string.length; lenOfX++) {
+            const lenOfY = (string.length - lenOfX * counts['x']) / counts['y'];
+            if (lenOfY <= 0 || lenOfY % 1 !== 0) continue;
+            const yIdx = firstYPos * lenOfX;
+            const x = string.slice(0, lenOfX);
+            const y = string.slice(yIdx, yIdx + lenOfY);
+            const potentialMatch = newPattern.map(char => (char === 'x' ? x : y));
+            if (string === potentialMatch.join('')) {
+
+                return !didSwitch ? [x,y] : [y,x];
+            }
+        }
+    }
 }
