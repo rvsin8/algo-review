42328f10061c3b3125e8d51540ea4789f4ab4fb2
rvsin8 <rsingh0815@gmail.com>
2021-04-14 12:31:00 -0400

    pattern matcher

--- Desktop/algoexpert/hard/pattern_matcher.js
+++ Desktop/algoexpert/hard/pattern_matcher.js
@@ -9,3 +9,10 @@
 //space complexity
 //O(n + m)
 
+function patternMatch(pattern, string) {
+    if (pattern.length > string.length) return [];
+    const newPattern = getNewPattern(pattern);
+    const didSwitch = newPattern[0] !== pattern[0];
+    const counts = {x: 0, y: 0};
+    const firstYPos = getCountsAndFirstYPos(newPattern, counts);
+}
