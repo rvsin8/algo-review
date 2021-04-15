e57e0821793c6186472d95c7b4437ecc3d414f8e
rvsin8 <rsingh0815@gmail.com>
2021-04-15 16:47:12 -0400

    max path sum in binary tree

--- Desktop/algoexpert/hard/max_path_sum_bt.js
+++ Desktop/algoexpert/hard/max_path_sum_bt.js
@@ -26,7 +26,7 @@
 //O(n) where n is the total number of nodes in a tree, so we traverse every node aka n nodes 
 
 //space complexity 
-//O(log(n)) where n is th total number of nodes bc we are dealing with a binary tree, when we call the recursive call aka log of n calls
+//O(log(n)) where n is th total number of nodes bc we are dealing with a binary tree, when we call the recursive call aka log of n calls assuming we have a primarily balanced bt
 
 function maxPathSum(tree) {
     const [_, maxSum] = findMaxSum(tree);
