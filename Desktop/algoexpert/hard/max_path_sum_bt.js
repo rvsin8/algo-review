d89e18bbd39104f882dcef0fa58d88e7fb374398
rvsin8 <rsingh0815@gmail.com>
2021-04-15 14:44:20 -0400

    max path sum in binary tree

--- Desktop/algoexpert/hard/max_path_sum_bt.js
+++ Desktop/algoexpert/hard/max_path_sum_bt.js
@@ -24,4 +24,8 @@ function findMaxSum(tree) {
     const {value} = tree;
     const maxSumAsBranch = Math.max(maxChildSumAsBranch + value, value);
     const maxSumAsRootNode = Math.max(leftMaxSumAsBranch + value + rightMaxSumAsBranch, maxSumAsBranch);
+    const maxPathSum = Math.max(leftMaxPathSum, rightMaxPathSum, maxSumAsRootNode);
+
+    return [maxSumAsBranch, maxPathSum];
+
 }
