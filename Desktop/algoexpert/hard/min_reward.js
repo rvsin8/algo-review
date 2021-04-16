7ac77a1478c39284d91b9b3432d530fc5fe191c2
rvsin8 <rsingh0815@gmail.com>
2021-04-16 16:54:50 -0400

    min rewards

--- Desktop/algoexpert/hard/min_reward.js
+++ Desktop/algoexpert/hard/min_reward.js
@@ -4,6 +4,17 @@
 //my understandings 
 
 //time complexity 
+//
 
 //space complexity 
 
+function minRewards(scores) {
+    const rewards = scores.map(_ => 1);
+    for (let i = 1; i < scores.length; i++) {
+        if (scores[i] > scores[i - 1]) rewards[i] = rewards[i-1] + 1;
+    }
+    for (let i = scores.length - 2; i >= 0; i--) {
+        if (scores[i] > scores[i+1]) rewards[i] = Math.max(rewards[i], rewards[i+1] + 1 );
+    }
+    return rewards.reduce((a,b) => a + b);
+}
