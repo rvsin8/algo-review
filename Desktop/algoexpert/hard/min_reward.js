6384c728297adb6b618cf9226c83306d8f1a5ae7
rvsin8 <rsingh0815@gmail.com>
2021-04-19 14:27:03 -0400

    min rewards

--- Desktop/algoexpert/hard/min_reward.js
+++ Desktop/algoexpert/hard/min_reward.js
@@ -9,6 +9,10 @@
 //[4,3,2,1,2,3,4,5,1], 25
 //if we keep iterating through the array, we will probably need to iterate back to update some scores
 //when we are iterating back, the rewards array at idx j --> rewards[i] = max {rewards[j] pr rewards[j+1]+1}
+//if you iterate from left to right, when you got a score that is greater than the previous one you just give it the rewards that the previous one had + 1, if it is smaller than the previous one then you give it 1 and start iterating it back, then you assign the max we already we assigned or whatever they got + 1 and pick the max of that
+//that was the naive approach --> O(n^2) time and O(n)
+
+//optimal solution
 
 
 //time complexity 
