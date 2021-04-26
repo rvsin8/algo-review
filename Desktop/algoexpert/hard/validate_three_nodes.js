bdd95e194531a3310257accd2964163c62bcd5df
rvsin8 <rsingh0815@gmail.com>
2021-04-26 12:40:19 -0400

    validate three nodes

--- Desktop/algoexpert/hard/validate_three_nodes.js
+++ Desktop/algoexpert/hard/validate_three_nodes.js
@@ -4,8 +4,11 @@
 //my understanding
 //we are given three inputs and these inputs are individual nodes contained in the same binary search tree
 //these nodes are labeled node one, node two and node three 
-//what we need to try to find out is if node 2 is btw node 1 and 3
-
+//what we need to try to find out is if node 2 is btw node 1 and 3, are they ancestors of node 2
+//if one is the descendant we need to check if the other is an ancestor
+//return true if it is true
+//these nodes will be unique and not null
+//
 
 //time complexity 
 
