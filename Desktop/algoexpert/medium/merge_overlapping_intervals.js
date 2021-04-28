1a597df51da418e20b2244590f534bc71bc81146
rvsin8 <rsingh0815@gmail.com>
2021-04-28 18:23:07 -0400

    merge overlapping intervals

--- Desktop/algoexpert/medium/merge_overlapping_intervals.js
+++ Desktop/algoexpert/medium/merge_overlapping_intervals.js
@@ -11,7 +11,12 @@ function mergeOverlappingIntervals(intervals) {
     const sortedIntervals = intervals.sort((a,b) => a[0] - b[0]);
 
     const mergedIntervals = [];
-    let currentInterval = sortedInterval[0];
+    let currentInterval = sortedIntervals[0];
     mergedIntervals.push(currentInterval);
+
+    for (const nextInterval of sortedIntervals) {
+        const [_, currentIntervalEnd] = currentInterval;
+        const [nextIntervalStart, nextIntervalEnd] = nextInterval;
+    }
 
 }
