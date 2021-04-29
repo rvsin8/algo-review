b757a952e37327c5d1c4c6677e8b3a68023029fd
rvsin8 <rsingh0815@gmail.com>
2021-04-28 20:59:46 -0400

    knapsack problem

--- Desktop/algoexpert/hard/knapsack_problem.js
+++ Desktop/algoexpert/hard/knapsack_problem.js
@@ -15,4 +15,11 @@ function knapsackProblem(items, capacity) {
         const row = new Array(capacity + 1).fill(0);
         knapsackValues.push(row);
     }
+    for (i = 1; i < items.length + 1; i++) {
+        const currentWeight = items[i - 1][1];
+        const currentValue = items[i - 1][0];
+        for (let c = 0; c < capacity + 1; c++) {
+            
+        }
+    }
 }
