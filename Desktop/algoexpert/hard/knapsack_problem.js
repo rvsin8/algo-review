2c5da49b318b05cac0eb0a6a9a48b4adcd511594
rvsin8 <rsingh0815@gmail.com>
2021-04-28 21:02:02 -0400

    knapsack problem

--- Desktop/algoexpert/hard/knapsack_problem.js
+++ Desktop/algoexpert/hard/knapsack_problem.js
@@ -19,7 +19,14 @@ function knapsackProblem(items, capacity) {
         const currentWeight = items[i - 1][1];
         const currentValue = items[i - 1][0];
         for (let c = 0; c < capacity + 1; c++) {
-            
+            if (currentWeight > c) {
+                knapsackValues[i][c] = knapsackValues[i - 1][c];
+            } else {
+                knapsackValues[i][c] = Math.max (
+                    knapsackValues[i - 1][c],
+                    knapsackValues[i - 1][c - currentWeight] + currentValue,
+                );
+            }
         }
     }
 }
