4b20dc90ea18bb30bb9eb19a9f6d0bf6e144383f
rvsin8 <rsingh0815@gmail.com>
2021-04-28 20:58:36 -0400

    knapsack problem

--- Desktop/algoexpert/hard/knapsack_problem.js
+++ Desktop/algoexpert/hard/knapsack_problem.js
@@ -9,3 +9,10 @@
 //space complexity 
 //O(nc)
 
+function knapsackProblem(items, capacity) {
+    const knapsackValues = [];
+    for (let i = 0; i < items.length + 1; i++) {
+        const row = new Array(capacity + 1).fill(0);
+        knapsackValues.push(row);
+    }
+}
