23b2f542daab604da9ebfc37082fd3cef6667e52
rvsin8 <rsingh0815@gmail.com>
2021-05-03 11:04:56 -0400

    cycle in graph

--- Desktop/algoexpert/medium/cycle_in_graph.js
+++ Desktop/algoexpert/medium/cycle_in_graph.js
@@ -13,4 +13,13 @@ function cycleInGraph(edges) {
     const numberOfNodes = edges.length;
     const visited = new Array(numberOfNodes).fill(false);
     const currentlyInStack = new Array(numberOfNodes).fill(false);
+
+    for (let node = 0; node < numberOfNodes; node++) {
+        if (visited[nodes]) continue;
+
+        const containsCycle = isNodeInCycle(node, edges, visited, currentlyInStack);
+        if (containsCycle) return true;
+    }
+
+    return false;
 }
