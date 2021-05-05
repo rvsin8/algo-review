beca0d80c0659b477dcfcb1547d40061bada9ca6
rvsin8 <rsingh0815@gmail.com>
2021-05-05 14:06:39 -0400

    sort stack

--- Desktop/algoexpert/medium/sort_stack.js
+++ Desktop/algoexpert/medium/sort_stack.js
@@ -5,6 +5,8 @@
 //in this question we are given one input which will be an array filled with integers
 //we must treat this array like a stack so we are only allowed to append element into the array or pop them off the array
 //you can only do these functions towards the end of the array aka the top of the stack
+//in place
+//we cant use sorting algorithms bc we cannot access indices 
 
 //time complexity 
 //O(n^2)
