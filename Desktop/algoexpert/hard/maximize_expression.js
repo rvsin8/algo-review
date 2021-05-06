f02da51933cf1b169446c62960e9f992c9e96115
rvsin8 <rsingh0815@gmail.com>
2021-05-06 11:31:01 -0400

    maximize expressions

--- Desktop/algoexpert/hard/maximize_expression.js
+++ Desktop/algoexpert/hard/maximize_expression.js
@@ -13,4 +13,7 @@ function maximizeExpression(array) {
     if (array.length < 4) return 0;
 
     const maxOfA = new Array(1).fill(array[0]);
+    const maxOfAMinusB = new Array(1).fill(-Infinity);
+    const maxOfAMinusBPlusC = new Array(2).fill(-Infinity);
+    const maxOfAMinusBPlusCMinusD = new Array(3).fill(-Infinity);
 }
