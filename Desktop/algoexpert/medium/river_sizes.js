bb9139690bbd1d3118a7771bad421b63fa4cd3a0
rvsin8 <rsingh0815@gmail.com>
2021-05-10 19:16:45 -0400

    river sizes

--- Desktop/algoexpert/medium/river_sizes.js
+++ Desktop/algoexpert/medium/river_sizes.js
@@ -10,4 +10,11 @@
 function riverSizes(matrix) {
     const sizes = [];
     const visited = matrix.map(row => row.map(value => false));
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (visited[i][j]) continue;
+            traverseNode(i, j, matrix, visited, sizes);
+        }
+    }
+    return sizes;
 }
