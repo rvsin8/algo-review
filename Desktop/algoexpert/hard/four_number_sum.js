a34683ce7e1f581d5d65adc812f86491e530e23b
rvsin8 <rsingh0815@gmail.com>
2021-05-11 19:58:54 -0400

    four number sums

--- Desktop/algoexpert/hard/four_number_sum.js
+++ Desktop/algoexpert/hard/four_number_sum.js
@@ -12,5 +12,11 @@
 function fourNumberSum(array, targetSum) {
     const allPairSums = {};
     const quadruplets = [];
+    for (let i = 1; i < array.length - 1; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            const currentSum = array[i] + array[j];
+            const difference = targetSum - currentSum;
+        }
+    }
 }
 
