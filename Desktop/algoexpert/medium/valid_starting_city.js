2effdd3b9d1fb80586fd187dde86330080b8ae97
rvsin8 <rsingh0815@gmail.com>
2021-05-12 00:50:06 -0400

    four number sums

--- Desktop/algoexpert/medium/valid_starting_city.js
+++ Desktop/algoexpert/medium/valid_starting_city.js
@@ -11,5 +11,9 @@
 
 
 function validStartingCity(distance, fuel, mpg) {
-    
+    const numberOfCities = distances.length;
+    let milesRemaining = 0;
+
+    let indexOfStartingCityCandidates = 0;
+    let milesRemainingAtStartingCityCandidate = 0;
 }
