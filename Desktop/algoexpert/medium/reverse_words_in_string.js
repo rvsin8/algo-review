e81c8a6ca7051606ed21afcceafc889f9b730349
rvsin8 <rsingh0815@gmail.com>
2021-05-13 00:50:18 -0400

    knapsack problem

--- Desktop/algoexpert/medium/reverse_words_in_string.js
+++ Desktop/algoexpert/medium/reverse_words_in_string.js
@@ -5,7 +5,11 @@
 //given a string and return the same string with all the words reverse 
 //"AlgoExpert is the best!" --> "best! the is AlgoExpert"
 //we want to keep the spaces the same in the reverse string
-//
+//we have four words, the ! is still apart of a word
+//what defines a word here is any characters grouped together and not separated by white space
+//you are not guaranteed to have any words in your string
+//"     " is valid
+//"   a" --> "a   "
 
 //time complexity 
 //O(n)
