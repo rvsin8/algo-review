7a6ccd2b47c09e12abc81e87375cdc28f92b25c4
rvsin8 <rsingh0815@gmail.com>
2021-05-12 21:49:19 -0400

    min num of coins for change

--- Desktop/algoexpert/hard/knapsack_problem.js
+++ Desktop/algoexpert/hard/knapsack_problem.js
@@ -22,16 +22,16 @@
 //O(nc) because we are building a 2D array
 
 function knapsackProblem(items, capacity) {
-    const knapsackValues = [];
-    for (let i = 0; i < items.length + 1; i++) {
-        const row = new Array(capacity + 1).fill(0);
-        knapsackValues.push(row);
+    const knapsackValues = []; //building our 2D array
+    for (let i = 0; i < items.length + 1; i++) { //top row
+        const row = new Array(capacity + 1).fill(0); //fill rows with 0
+        knapsackValues.push(row); //
     }
-    for (i = 1; i < items.length + 1; i++) {
-        const currentWeight = items[i - 1][1];
-        const currentValue = items[i - 1][0];
-        for (let c = 0; c < capacity + 1; c++) {
-            if (currentWeight > c) {
+    for (i = 1; i < items.length + 1; i++) { //for loop
+        const currentWeight = items[i - 1][1]; //we have that first row with no items 
+        const currentValue = items[i - 1][0]; //first value
+        for (let c = 0; c < capacity + 1; c++) { //for capacity
+            if (currentWeight > c) { //
                 knapsackValues[i][c] = knapsackValues[i - 1][c];
             } else {
                 knapsackValues[i][c] = Math.max (
