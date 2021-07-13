e706f4ccab0d4ca3f6bbce4eaaf0728300266099
rvsin8 <rsingh0815@gmail.com>
2021-07-13 16:14:31 -0400

    intersection

---
+++ Desktop/structy/intersection.js
@@ -0,0 +1,4 @@
+intersection
+Write a function, intersection, that takes in two arrays, a,b, as arguments. The function should return a new array containing elements that are in both of the two arrays.
+
+You may assume that each input array does not contain duplicate elements.
