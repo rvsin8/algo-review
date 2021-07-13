5aada601cb1ab604d8cccf8a16c132741f7a69be
rvsin8 <rsingh0815@gmail.com>
2021-07-13 15:10:21 -0400

    pair sums

--- Desktop/structy/pairsum.js
+++ Desktop/structy/pairsum.js
@@ -4,3 +4,13 @@ Write a function, pairSum, that takes in an array and a target sum as arguments.
 Be sure to return the indices, not the elements themselves.
 
 There is guaranteed to be one such pair that sums to the target.
+
+const pairSum = (numbers, targetSum) => {
+  const previousNums = {};
+  for (let i = 0; i < numbers.length; i += 1) {
+    const num = numbers[i];
+    const complement = targetSum - num;
+    if (complement in previousNums) return [i, previousNums[complement]]
+    previousNums[num] = i;
+  }
+};
