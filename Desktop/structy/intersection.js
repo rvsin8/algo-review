19dffae15360ea1bbf133e45a729235884a1138e
rvsin8 <rsingh0815@gmail.com>
2021-07-14 17:51:51 -0400

    intersection

--- Desktop/structy/intersection.js
+++ Desktop/structy/intersection.js
@@ -2,3 +2,14 @@ intersection
 Write a function, intersection, that takes in two arrays, a,b, as arguments. The function should return a new array containing elements that are in both of the two arrays.
 
 You may assume that each input array does not contain duplicate elements.
+
+const intersection = (a, b) => {
+  result = [];
+  const setA = new Set(a);
+  for (let item of b) {
+    if (setA.has(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+};
