4a65ef70a3d9c29e5300804b1cfbc9478d293b5e
rvsin8 <rsingh0815@gmail.com>
2021-08-04 19:11:00 -0400

    fixed USA as default

--- Desktop/structy/five_sort.js
+++ Desktop/structy/five_sort.js
@@ -1,6 +1,6 @@
 five sort
-Write a function, fiveSort, that takes in an array of numbers as an argument. The function should rearrange elements of the array such that all 5s appear at the end. Your function should perform this operation in-place by mutating the original array. The function should return the array.
+Write a function, fiveSort, that takes in an array of numbers as an argument.The function should rearrange elements of the array such that all 5s appear at the end.Your function should perform this operation in -place by mutating the original array.The function should return the array.
 
 Elements that are not 5 can appear in any order in the output, as long as all 5s are at the end of the array.
 
-Update for git - part 2
+Update for git - part 3
